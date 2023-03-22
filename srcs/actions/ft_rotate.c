fbe34474c381c2562bb4c37f4cd1169692c41e2f
Basil ALBADER <baalbade@student.42.fr>
2023-03-22 23:43:02 +0100

    actions: ft_rotate.c

--- srcs/actions/ft_rotate.c
+++ srcs/actions/ft_rotate.c
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+void	ft_rotate(t_list **stack)
+{
+	t_list	*temp;
+
+	temp = NULL;
+	if (!(*stack))
+		return ;
+	temp = *stack;
+	temp = temp->next;
+	(*stack)->next = NULL;
+	ft_add_new_tail(&temp, *stack);
+	(*stack) = temp;
+}
