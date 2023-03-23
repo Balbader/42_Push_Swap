3a039be9d8930fd8924b22815b62425ee16fb96a
Basil ALBADER <baalbade@student.42.fr>
2023-03-23 02:42:56 +0100

    sorting_utils: ft_create_node.c

---
+++ srcs/sorting_utils/ft_create_node.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_node.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 02:34:10 by baalbade          #+#    #+#             */
+/*   Updated: 2023/03/23 02:34:19 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*ft_create_node(int data, int index)
+{
+	t_list	*new_node;
+
+	new_node = NULL;
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->data = data;
+	new_node->index = index;
+	new_node->next = NULL;
+	return (new_node);
+}
