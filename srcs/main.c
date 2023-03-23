759481b45b902be5531ce72c4f0dc36b09e443c3
Basil ALBADER <baalbade@student.42.fr>
2023-03-23 03:15:48 +0100

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -44,9 +44,10 @@ int	main(int ac, char **av)
 	entries_arr = ft_init_entries_arr(ac, av, entries_arr);
 	a = ft_init_stack(a, entries_arr, ac - 1);
 	ft_print_stack(&a, "a");
-	ft_sort_3(&a);
+	ft_push_first_2(&a, &b);
 	ft_re_init_index(a);
 	ft_print_stack(&a, "a");
+	ft_print_stack(&b, "b");
 	ft_free_stack(&a);
 	free(entries_arr);
 	ft_free_stack(&b);
