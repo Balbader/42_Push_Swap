cbcaef07a79aeabf4cc9d63f90654eb928654fd3
Basil ALBADER <baalbade@student.42.fr>
2023-03-24 15:16:03 +0100

    sorting: ft_calculate_cost.c

--- srcs/sorting/ft_calculate_cost.c
+++ srcs/sorting/ft_calculate_cost.c
@@ -12,6 +12,26 @@
 
 #include "push_swap.h"
 
-int	ft_calculate_cost(t_list *stack, int node_idx)
+void	ft_calculate_cost(t_list **a)
 {
+	int	cheapest;
+	int	*data_arr;
+	int	*idx_arr;
+	int	a_size;
+	int	mid;
+	int	i;
+
+	data_arr = ft_init_data_array(a);
+	idx_arr = ft_init_idx_array(a);
+	a_size = ft_get_stack_size(*a);
+	mid = a_size / 2;
+	printf("a_size: %d\n", a_size);
+	printf("mid: %d\n", mid);
+	printf("mid_idx[%d]: %d\n", idx_arr[mid], data_arr[mid]);
+	i = 0;
+	while (i < a_size)
+	{
+		printf("[%d] : idx[%d] -> data[%d]\n", i, idx_arr[i], data_arr[i]);
+		++i;
+	}
 }
