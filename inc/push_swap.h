8ea3c521fc4803caf125fd39920f7b9eefea2825
Basil ALBADER <baalbade@student.42.fr>
2023-03-24 16:38:41 +0100

    .h: add 't_list **b' to arg of ft_check_incoming.c && ft_calculate_cost.c

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -54,11 +54,11 @@ void		ft_find_doubles(int *arr, int arr_size);
 long int	ft_convert_entry(const char *av);
 
 /* SORTING */
-int			ft_check_incoming(t_list **a, int data);
+int			ft_check_incoming(t_list **b, int data);
 int			*ft_init_cost_arr(t_list **a);
 int			*ft_init_data_array(t_list **a);
 int			*ft_init_idx_array(t_list **a);
-void		ft_calculate_cost(t_list **a);
+void		ft_calculate_cost(t_list **a, t_list **b);
 void		ft_push_first_2(t_list **a, t_list **b);
 void		ft_push_from_a_to_b(t_list **a, t_list **b);
 void		ft_sort_3(t_list **a);
