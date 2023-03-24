19ef9d9edc383a670595e0cda1939825953c5202
Basil ALBADER <baalbade@student.42.fr>
2023-03-24 16:38:50 +0100

    WIP

--- srcs/sorting/ft_check_incoming.c
+++ srcs/sorting/ft_check_incoming.c
@@ -12,29 +12,31 @@
 
 #include "push_swap.h"
 
-static int	ft_check_if_smallest(t_list **a, int data)
+static int	ft_check_if_smallest(t_list **b, int data)
 {
 	int	sml_data;
 
-	sml_data = ft_find_smallest_node_data(a);
-	if (data == sml_data)
+	sml_data = ft_find_smallest_node_data(b);
+	printf("sml_b: %d\n", sml_data);
+	if (data < sml_data)
 		return (1);
 	return (0);
 }
 
-static int	ft_check_if_biggest(t_list **a, int data)
+static int	ft_check_if_biggest(t_list **b, int data)
 {
 	int	big_data;
 
-	big_data = ft_find_biggest_node_data(a);
-	if (data == big_data)
+	big_data = ft_find_biggest_node_data(b);
+	printf("big_b: %d\n", big_data);
+	if (data > big_data)
 		return (1);
 	return (0);
 }
 
-int	ft_check_incoming(t_list **a, int data)
+int	ft_check_incoming(t_list **b, int data)
 {
-	if (ft_check_if_biggest(a, data) == 1 || ft_check_if_smallest(a, data) == 1)
+	if (ft_check_if_biggest(b, data) == 1 || ft_check_if_smallest(b, data) == 1)
 		return (1);
 	return (0);
 }
