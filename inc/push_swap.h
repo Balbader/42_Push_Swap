0ccf9c79e0694dde8ac881a59b59de332ec20168
Basil ALBADER <baalbade@student.42.fr>
2023-03-25 19:54:35 +0100

    .h: add ft_find_closest_node_idx.c fct prototype

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -50,6 +50,7 @@ void		ft_ss(t_list **a, t_list **b);
 int			*ft_init_a_cost_arr(t_list **a);
 // int			*ft_init_b_cost_arr(t_list **a, t_list **b);
 int			ft_init_b_cost_arr(t_list **a, t_list **b);
+int			ft_find_closest_node_idx(t_list **b, int incoming);
 int			*ft_init_cost_arr(t_list **stack);
 int			*ft_init_data_array(t_list **stack);
 int			*ft_init_idx_array(t_list **stack);
