49b63f5d49a0dc59add561f2d5b4d2a6a4ddd47a
Basil ALBADER <baalbade@student.42.fr>
2023-03-25 18:24:37 +0100

    cost: ft_init_b_cost_arr.c : wip

--- srcs/cost/ft_init_b_cost_arr.c
+++ srcs/cost/ft_init_b_cost_arr.c
@@ -12,11 +12,101 @@
 
 #include "push_swap.h"
 
+static int	ft_cost_biggest_b(t_list **b)
+{
+	int	biggest_idx;
+	int	b_size;
+	int	mid;
+
+	biggest_idx = ft_find_biggest_node_idx(b);
+	b_size = ft_get_stack_size(*b);
+	if (b_size > 2)
+	{
+		mid = b_size / 2;
+		if (biggest_idx <= mid)
+			return (biggest_idx - 1);
+		else if (biggest_idx > mid)
+			return (biggest_idx);
+	}
+	return (0);
+}
+
+static int	ft_find_closest_node_idx(t_list **b, int data)
+{
+	t_list	*tmp;
+	int		closest_data;
+	int		closest_idx;
+
+	tmp = NULL;
+	tmp = (*b);
+	closest_data = tmp->data;
+	closest_idx = 0;
+	while (tmp)
+	{
+		if (tmp->data == data - 1)
+		{
+			closest_data = tmp->data;
+			closest_idx = tmp->index;
+		}
+		tmp = tmp->next;
+	}
+	// printf("closest_data: %d\n", closest_data);
+	// printf("closest_idx: %d\n", closest_idx);
+	return (free(tmp), closest_idx);
+}
+
+static int	ft_cost_closest_b(t_list **b, int closest_idx)
+{
+	int	b_size;
+	int	mid;
+
+	b_size = ft_get_stack_size(*b);
+	mid = b_size / 2;
+	if (closest_idx <= mid)
+		return (closest_idx - 1);
+	else if (closest_idx > mid)
+		return (closest_idx);
+	return (0);
+}
+
+int	ft_init_b_cost_arr(t_list **a, t_list **b)
+{
+	int	closest_idx;
+	int	closest_idx_cost;
+	int	biggest_b_cost;
+
+	(void)a;
+	ft_re_init_index(*b);
+	biggest_b_cost = ft_cost_biggest_b(b);
+	closest_idx = ft_find_closest_node_idx(b, 18);
+	closest_idx_cost = ft_cost_closest_b(b, closest_idx);
+	printf("biggest_b_cost: %d\n", biggest_b_cost);
+	printf("closest_idx: %d\n", closest_idx);
+	printf("closest_idx_cost: %d\n", closest_idx_cost);
+	return (closest_idx_cost);
+}
+
+/*
 int	*ft_init_b_cost_arr(t_list **a, t_list **b)
 {
 	int	*b_cost_arr;
+	int	*a_data_log;
+	int	incoming;
+	int	a_size;
+	int	i;
 
-	(void)b;
+	a_data_log = ft_init_data_array(a);
 	b_cost_arr = ft_init_cost_arr(a);
+	a_size = ft_get_stack_size(*a);
+	i = 0;
+	while (i < a_size)
+		{
+			incoming = ft_check_incoming(b, a_data_log[i]);
+			if (incoming == 1)
+				ft_get_biggest_b(b);
+			else
+			++i;
+		}
 	return (b_cost_arr);
 }
+*/
