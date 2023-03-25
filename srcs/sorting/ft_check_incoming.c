6d4d4ad437d10f3f8045d12dc52a802757773838
Basil ALBADER <baalbade@student.42.fr>
2023-03-25 08:40:25 +0100

    sorting: update printing of 'big_b' && 'sml_b' in ft_check_incoming.c

--- srcs/sorting/ft_check_incoming.c
+++ srcs/sorting/ft_check_incoming.c
@@ -17,7 +17,6 @@ static int	ft_check_if_smallest(t_list **b, int data)
 	int	sml_data;
 
 	sml_data = ft_find_smallest_node_data(b);
-	printf("sml_b: %d\n", sml_data);
 	if (data < sml_data)
 		return (1);
 	return (0);
@@ -28,7 +27,6 @@ static int	ft_check_if_biggest(t_list **b, int data)
 	int	big_data;
 
 	big_data = ft_find_biggest_node_data(b);
-	printf("big_b: %d\n", big_data);
 	if (data > big_data)
 		return (1);
 	return (0);
@@ -36,7 +34,21 @@ static int	ft_check_if_biggest(t_list **b, int data)
 
 int	ft_check_incoming(t_list **b, int data)
 {
-	if (ft_check_if_biggest(b, data) == 1 || ft_check_if_smallest(b, data) == 1)
+	int	big_data;
+	int	sml_data;
+
+	sml_data = ft_find_smallest_node_data(b);
+	big_data = ft_find_biggest_node_data(b);
+
+	if (ft_check_if_biggest(b, data) == 1)
+	{
+		printf("big_b: %d\n", big_data);
 		return (1);
+	}
+	if (ft_check_if_smallest(b, data) == 1)
+	{
+		printf("sml_b: %d\n", sml_data);
+		return (1);
+	}
 	return (0);
 }
