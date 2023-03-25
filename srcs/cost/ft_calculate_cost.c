2c399d3605419f77517ec600c1679e53a5c335f6
Basil ALBADER <baalbade@student.42.fr>
2023-03-25 19:26:14 +0100

    cost: wip

--- srcs/cost/ft_calculate_cost.c
+++ srcs/cost/ft_calculate_cost.c
@@ -15,24 +15,14 @@
 void	ft_calculate_cost(t_list **a, t_list **b)
 {
 	// int	cheapest; -> this will be returned value
-	// int	big_or_sml;
 	int	a_size;
-	// int	mid;
-	// int	i;
 	int	*a_cost_arr;
+	int	closest_idx_cost;
 
-	(void)b;
 	a_size = ft_get_stack_size(*a);
-	// mid = a_size / 2;
-	// i = 0;
-	// while (i < a_size)
-	// {
-	// 	big_or_sml = ft_check_incoming(b, data_arr[i]);
-	// 	printf("big_or_sml: %d - a: %d\n\n", big_or_sml, data_arr[i]);
-	// 	++i;
-	// }
+	closest_idx_cost = ft_init_b_cost_arr(a, b);
+	printf("closest_idx_cost: %d\n", closest_idx_cost);
 	a_cost_arr = ft_init_a_cost_arr(a);
-	for (int i = 0; i < a_size; ++i) {
-		printf("a_cost_arr[%d]: %d\n", i, a_cost_arr[i]);
-	}
+	ft_print_stack(b, "b");
+	free(a_cost_arr);
 }
