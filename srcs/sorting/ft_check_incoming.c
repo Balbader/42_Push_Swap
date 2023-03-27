c39f7959b7acb086796b4561fae164bdd4f4ffdd
Basil ALBADER <baalbade@student.42.fr>
2023-03-27 10:00:43 +0200

    sorting: ft_check_incoming.c clean up

--- srcs/sorting/ft_check_incoming.c
+++ srcs/sorting/ft_check_incoming.c
@@ -40,14 +40,8 @@ int	ft_check_incoming(t_list **b, int data)
 	sml_data = ft_find_smallest_node_data(b);
 	big_data = ft_find_biggest_node_data(b);
 	if (ft_check_if_biggest(b, data) == 1)
-	{
-		printf("big_b: %d\n", big_data);
 		return (1);
-	}
 	if (ft_check_if_smallest(b, data) == 1)
-	{
-		printf("sml_b: %d\n", sml_data);
 		return (1);
-	}
 	return (0);
 }
