9d7924c19c8eef8bef6eb1809f8f63ea0a8b9a2b
Basil ALBADER <baalbade@student.42.fr>
2023-03-27 17:19:41 +0200

    Need to update astrovim config

--- srcs/main.c
+++ srcs/main.c
@@ -12,45 +12,40 @@
 
 #include "push_swap.h"
 
-static int	*ft_init_entries_arr(int ac, char **av, int *arr)
-{
-	int	i;
-	int	j;
+static int *ft_init_entries_arr(int ac, char **av, int *arr) {
+  int i;
+  int j;
 
-	arr = NULL;
-	arr = (int *)malloc(sizeof(int) * (ac - 1));
-	if (!arr)
-		return (0);
-	j = 1;
-	i = 0;
-	while (i < ac - 1)
-	{
-		arr[i] = ft_get_entry(av[j]);
-		++i;
-		++j;
-	}
-	return (arr);
+  arr = NULL;
+  arr = (int *)malloc(sizeof(int) * (ac - 1));
+  if (!arr)
+    return (0);
+  j = 1;
+  i = 0;
+  while (i < ac - 1) {
+    arr[i] = ft_get_entry(av[j]);
+    ++i;
+    ++j;
+  }
+  return (arr);
 }
 
-int	main(int ac, char **av)
-{
-	t_list	*a;
-	t_list	*b;
-	int		*entries_arr;
+int main(int ac, char **av) {
+  t_list *a;
+  t_list *b;
+  int nb;
+  int *entries_arr;
 
-	a = NULL;
-	b = NULL;
-	entries_arr = NULL;
-	entries_arr = ft_init_entries_arr(ac, av, entries_arr);
-	a = ft_init_stack(a, entries_arr, ac - 1);
-	// ft_print_stack(&a, "a");
-	ft_push_first_2(&a, &b);
-	ft_re_init_index(a);
-	ft_push_from_a_to_b(&a, &b);
-	// ft_print_stack(&a, "a");
-	// ft_print_stack(&b, "b");
-	ft_free_stack(&a);
-	free(entries_arr);
-	ft_free_stack(&b);
-	return (0);
+  a = NULL;
+  b = NULL;
+  entries_arr = NULL;
+  entries_arr = ft_init_entries_arr(ac, av, entries_arr);
+  a = ft_init_stack(a, entries_arr, ac - 1);
+  ft_push_first_2(&a, &b);
+  ft_re_init_index(a);
+  ft_push_from_a_to_b(&a, &b);
+  ft_free_stack(&a);
+  free(entries_arr);
+  ft_free_stack(&b);
+  return (0);
 }
