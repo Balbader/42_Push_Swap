ed3135932166382f54bc1e94c50854b66ba08506
Balbader <basilalbapro@gmail.com>
2023-03-30 04:16:36 +0200

    cost: ft_reg_instructions.c

---
+++ srcs/cost/ft_reg_instructions.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reg_instructions.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 03:49:56 by baalbade          #+#    #+#             */
+/*   Updated: 2023/03/30 03:50:00 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	*ft_reg_instructions(int a_cost, int b_cost)
+{
+	int	*reg;
+
+	reg = NULL;
+	reg = (int *)malloc(sizeof(int) * 2);
+	if (!reg)
+		return (0);
+	reg[0] = a_cost;
+	reg[1] = b_cost;
+	return (reg);
+}
