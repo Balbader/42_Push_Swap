ac3ccc77da7406e8e62524c9fb2843f18f71a82f
Basil ALBADER <baalbade@student.42.fr>
2023-03-30 14:06:28 +0200

    add ft_pb instruction to reg array

--- srcs/cost/ft_reg_instructions.c
+++ srcs/cost/ft_reg_instructions.c
@@ -17,10 +17,11 @@ int	*ft_reg_instructions(int a_cost, int b_cost)
 	int	*reg;
 
 	reg = NULL;
-	reg = (int *)malloc(sizeof(int) * 2);
+	reg = (int *)malloc(sizeof(int) * 3);
 	if (!reg)
 		return (0);
 	reg[0] = a_cost;
 	reg[1] = b_cost;
+	reg[2] = 1;
 	return (reg);
 }
