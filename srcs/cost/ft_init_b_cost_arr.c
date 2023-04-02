088905cbe58e0177b995e20354e0a7c424d96550
Balbader <basilalbapro@gmail.com>
2023-04-02 12:16:42 +0200

    clean up

--- srcs/cost/ft_init_b_cost_arr.c
+++ srcs/cost/ft_init_b_cost_arr.c
@@ -14,22 +14,19 @@
 
 static int	ft_cost_biggest_b(t_list **b)
 {
-	int	biggest_idx;
+	int	biggest_b_node_idx;
 	int	b_size;
 	int	mid;
 	int	cost;
 
 	cost = 0;
-	biggest_idx = ft_find_biggest_node_idx(b);
+	biggest_b_node_idx = ft_find_biggest_node_idx(b);
 	b_size = ft_get_stack_size(*b);
-	if (b_size > 2)
-	{
-		mid = b_size / 2;
-		if (biggest_idx <= mid)
-			cost = biggest_idx - 1;
-		if (biggest_idx > mid)
-			cost = ((b_size - biggest_idx) + 1) * -1;
-	}
+	mid = b_size / 2;
+	if (biggest_b_node_idx <= mid)
+		cost = biggest_b_node_idx - 1;
+	if (biggest_b_node_idx > mid)
+		cost = ((b_size - biggest_b_node_idx) + 1) * -1;
 	return (cost);
 }
 
@@ -50,34 +47,37 @@ static int	ft_cost_closest_b(t_list **b, int closest_idx)
 }
 
 /*
- * Returns an array containg the cost of bringing b_node to the top of 'b' for
- * each b_node based on the a_node coming in
+ * Returns an array containg the tot number or rb || rrb
+ * to bring the correct b_node to the top of 'b'
+ * based on if the incoming a_node is either:
+ * . == new smallest(b) || new biggest(b)
+ * . != new smallest(b) || new biggest(b)
 */
 int	*ft_init_b_cost_arr(t_list **a, t_list **b)
 {
-	int	*b_cost_arr;
-	int	*a_data_arr;
+	int	*b_cost;
+	int	*a_data;
 	int	a_size;
-	int	closest_idx;
+	int	closest_b_node_idx;
 	int	i;
 
 	a_size = ft_get_stack_size(*a);
-	a_data_arr = ft_init_data_array(a);
-	b_cost_arr = NULL;
-	b_cost_arr = (int *)malloc(sizeof(int) * a_size);
-	if (!b_cost_arr)
+	a_data = ft_init_data_array(a);
+	b_cost = NULL;
+	b_cost = (int *)malloc(sizeof(int) * a_size);
+	if (!b_cost)
 		return (0);
 	i = 0;
 	while (i < a_size)
 	{
-		if (ft_check_incoming(b, a_data_arr[i]) == 1)
-			b_cost_arr[i] = ft_cost_biggest_b(b);
-		if (ft_check_incoming(b, a_data_arr[i]) == 0)
+		if (ft_check_incoming(b, a_data[i]) == 1)
+			b_cost[i] = ft_cost_biggest_b(b);
+		if (ft_check_incoming(b, a_data[i]) == 0)
 		{
-			closest_idx = ft_find_closest_node_idx(b, a_data_arr[i]);
-			b_cost_arr[i] = ft_cost_closest_b(b, closest_idx);
+			closest_b_node_idx = ft_find_closest_b_node_idx(b, a_data[i]);
+			b_cost[i] = ft_cost_closest_b(b, closest_b_node_idx);
 		}
 		++i;
 	}
-	return (b_cost_arr);
+	return (b_cost);
 }
