fe29bc08bebfdbd4f008738cf6f770db687e4b5e
Basil ALBADER <baalbade@student.42.fr>
2023-04-03 07:39:12 +0200

    add function to calculate optimized cost for a_cost and b_cost

--- srcs/cost/ft_calculate_cost_arr.c
+++ srcs/cost/ft_calculate_cost_arr.c
@@ -24,6 +24,33 @@ static int	ft_check_cost(int cost)
 	return (cost);
 }
 
+static int	ft_optimize(int	a_cost, int b_cost)
+{
+	int	cheapest;
+
+	cheapest = 0;
+	if (ft_is_pos(a_cost, b_cost))
+	{
+		if (a_cost > b_cost)
+			cheapest = a_cost - b_cost;
+		if (b_cost > a_cost)
+			cheapest = b_cost - a_cost;
+	}
+	if (ft_is_neg(a_cost, b_cost))
+	{
+		if (a_cost < b_cost)
+			cheapest = b_cost - a_cost;
+		if (b_cost < a_cost)
+			cheapest = a_cost - b_cost;
+	}
+	return (cheapest);
+}
+
+/*
+ * 1. calculates the tot move counts to push a_node to 'b' for each a_node in 'a'
+ * 2. the result is the sum of a_cost + b_cost
+ * 3. the result does not include the last 'ft_pb'
+*/
 static int	*ft_calculate_cost(int *cost_arr, t_list **a, t_list **b)
 {
 	int		*a_cost_arr;
@@ -39,12 +66,10 @@ static int	*ft_calculate_cost(int *cost_arr, t_list **a, t_list **b)
 	while (i < a_size)
 	{
 		if ((ft_check_costs(a_cost_arr[i], b_cost_arr[i]) == 1))
-			cost_arr[i] = ft_optimize_cost(a_cost_arr[i], b_cost_arr[i]);
+			cost_arr[i] = ft_optimize(a_cost_arr[i], b_cost_arr[i]);
 		else
 			cost_arr[i] = ((ft_check_cost(a_cost_arr[i]))
 					+ ft_check_cost(b_cost_arr[i]));
-			// cost_arr[i] = ((ft_check_cost(a_cost_arr[i]))
-			// 		+ ft_check_cost(b_cost_arr[i]) + 1);
 		++i;
 	}
 	return (free(a_cost_arr), free(b_cost_arr), cost_arr);
