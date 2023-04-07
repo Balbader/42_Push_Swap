d340eee12898725878664eee1c83c33b64240446
balbader <basilalbapro@gmail.com>
2023-04-07 15:44:21 +0200

    main.c

--- working/srcs/main.c
+++ working/srcs/main.c
@@ -45,11 +45,10 @@ int	main(int ac, char **av)
 	a = ft_init_stack(a, entries_arr, ac - 1);
 	if (ac - 1 <= 5)
 		ft_small_sort(a, b, ac - 1);
-	else if (ac - 1 == 100 || ac - 1 == 500)
+	else
 		ft_pb_chunks(&a, &b);
 		// ft_sort_b_and_pa(&a, &b);
-	else
-		ft_sort_stack(&a, &b);
+		// ft_sort_stack(&a, &b);
 	ft_print_stack(&b, "b");
 	ft_free_stack(&a);
 	free(entries_arr);
