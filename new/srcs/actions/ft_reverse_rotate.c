23a615bbc7542c9170a0d60a2eff6cfaf696176d
balbader <basilalbapro@gmail.com>
2023-04-09 12:50:00 +0200

    actions fcts

---
+++ new/srcs/actions/ft_reverse_rotate.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reverse_rotate.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 23:23:12 by baalbade          #+#    #+#             */
+/*   Updated: 2023/03/22 23:23:14 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_reverse_rotate(t_list **stack)
+{
+	t_list	*tmp;
+	t_list	*last;
+	t_list	*before_last;
+
+	tmp = NULL;
+	last = NULL;
+	before_last = NULL;
+	last = ft_get_last_node(*stack);
+	before_last = ft_get_before_last_node(*stack);
+	tmp = *stack;
+	*stack = last;
+	(*stack)->next = tmp;
+	before_last->next = NULL;
+}
