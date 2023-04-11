18618df90c55c0cbd2592818b65f541589f6b308
Basil ALBADER <baalbade@student.42.fr>
2023-04-11 08:49:08 +0200

    .h

--- new/inc/push_swap.h
+++ new/inc/push_swap.h
@@ -23,10 +23,13 @@ typedef struct s_list
 {
 	int				data;
 	int				index;
+	int				tot_hits;
 	struct s_list	*next;
 }				t_list;
 
 /* ACTIONS */
+void		ft_find_cost_move(t_list **a, t_list **b);
+void		ft_best_move(t_list **a, t_list **b);
 void		ft_push_node(t_list **src, t_list **dest);
 void		ft_rotate(t_list **stack);
 void		ft_reverse_rotate(t_list **stack);
@@ -59,49 +62,48 @@ void		ft_print_stack(t_list **stack, char *name);
 void		ft_putstr_fd(char *str, int fd);
 
 /* STACK */
-
 int			ft_is_biggest_node(t_list **stack);
-int			ft_find_biggest_node_pos(t_list *stack);
-int			ft_find_smallest_node_pos(t_list *stack);
+int			ft_find_biggest_node_data(t_list *stack);
+int			ft_find_biggest_node_idx(t_list *stack);
+int			ft_find_smallest_node_data(t_list *stack);
+int			ft_find_smallest_node_idx(t_list *stack);
 int			ft_get_stack_size(t_list *stack);
 int			ft_stack_is_sorted(t_list *stack);
 int			*ft_copy_stack_index(t_list *stack);
 int			*ft_copy_stack_to_arr(t_list *stack, int stack_size, int *arr);
 
+void		ft_re_init_index(t_list *stack);
 void		ft_add_new_head(t_list **lst, t_list *node);
 void		ft_add_new_tail(t_list **lst, t_list *node);
 void		ft_def_stack_case(t_list *stack_a, t_list *stack_b);
 void		ft_free_stack(t_list **stack);
-void		ft_split_stack(t_list *stack_a, t_list *stack_b);
 
 t_list		*ft_create_stack_copy(t_list *a, t_list *copy);
 t_list		*ft_create_node(int data, int index);
 t_list		*ft_find_middle_node(t_list *stack);
 t_list		*ft_get_before_last_node(t_list *stack);
 t_list		*ft_get_last_node(t_list *stack);
-t_list		*ft_init_rand_stack(int *rand_arr, int start, int end);
 t_list		*ft_init_stack(t_list *stack_a, int *entries, int ac);
-t_list		*ft_re_init_index(t_list *stack);
 
 /* SORTING */
-t_list		*ft_sort_3(t_list *stack);
-void		ft_sort_4(t_list *stack_a, t_list *stack_b);
+void		ft_sort_3(t_list **stack);
+void		ft_sort_4(t_list **a, t_list **b);
 void		ft_finish_sorting_4(t_list **stack_a, t_list **stack_b);
-void		ft_sort_5(t_list *stack_a, t_list *stack_b);
+void		ft_sort_5(t_list **a, t_list **b);
 void		ft_finish_sorting_5(t_list **stack_a, t_list **stack_b);
-void		ft_small_sort(t_list *a, t_list *b, int arr_size);
+void		ft_small_sort(t_list **a, t_list **b, int arr_size);
 void		ft_sort_stack(t_list **a, t_list **b);
 void		ft_sort_b_and_pa(t_list **a, t_list **b);
 
 /* SORTING UTILS*/
 int			*ft_get_pivots(t_list **a);
-// int			ft_define_pivots_count(int pivots_count, int stack_size);
+int			ft_define_pivots_count(int pivots_count, int stack_size);
 int			*ft_find_pivots_idx(t_list **a, int *limits);
 int			**ft_find_all_path_len(t_list *stack, int *stack_cpy);
 void		ft_find_longest_path(t_list *stack, int *arr);
 void		ft_pb_chunks(t_list **a, t_list **b);
-void		ft_push_smallest_node_4(t_list **stack_a,
-				t_list **stack_b, int smallest_node_index);
+void		ft_push_smallest_node_4(t_list **a,
+							t_list **b, int smallest_node_index);
 t_list		*ft_pre_sort_stack(t_list *rand_stack);
 t_list		*ft_start_sorting(t_list **stack_a,
 				t_list **stack_b, int stack_size);
