9d033de050975bb0ba2c22d092ca4189e59bbc3b
balbader <basilalbapro@gmail.com>
2023-04-12 14:17:06 +0200

    WIP

--- final/srcs/a_to_b/ft_pb_chunks_utils.c
+++ final/srcs/a_to_b/ft_pb_chunks_utils.c
@@ -42,7 +42,6 @@ int	*ft_find_pivots_idx(t_list **a, int *pivots_idx)
 
 	stack_size = ft_get_stack_size(a);
 	pivots_count = 10;
-	// pivots_count = ft_define_pivots_count(pivots_count, stack_size);
 	pivots_idx = ft_fill_in_pivots_idx(pivots_idx, pivots_count, stack_size);
 	return (pivots_idx);
 }
