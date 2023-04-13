6aa7779d5cae587cbc3e2a06436b6efd5ebb1df9
Basil ALBADER <baalbade@student.42.fr>
2023-04-13 14:17:38 +0200

    add NULL security for stackk

--- final/srcs/cost/ft_big_or_next.c
+++ final/srcs/cost/ft_big_or_next.c
@@ -21,10 +21,10 @@ int	ft_big_or_next(int big_idx, int next_idx, t_list **stack)
 	int	big_cost;
 	int	next_cost;
 
-	if (!stack)
-		return (0);
 	big_cost = ft_calculate_cost(stack, big_idx);
 	next_cost = ft_calculate_cost(stack, next_idx);
+	if (!stack)
+		return (0);
 	if (big_cost > 0 && next_cost > 0)
 	{
 		if (big_cost < next_cost)
