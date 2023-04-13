e61a7eb7fb638075f59adcacba6e8032e578f9cd
Basil ALBADER <baalbade@student.42.fr>
2023-04-13 12:03:58 +0200

    fcts to calculate cost of moving biggest and next biggest node to top of stack b and find which one of the two is the cheapest to move

---
+++ final/srcs/cost/ft_big_or_next.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_big_or_next.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 11:15:38 by baalbade          #+#    #+#             */
+/*   Updated: 2023/04/13 11:15:40 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+ * returns 1 if biggest node is cheapest
+ * returns 2 if 2nd biggest node is cheapest
+*/
+int	ft_big_or_next(int big_idx, int next_idx, t_list **stack)
+{
+	int	big_cost;
+	int	next_cost;
+
+	big_cost = ft_calculate_cost(stack, big_idx);
+	next_cost = ft_calculate_cost(stack, next_idx);
+	if (big_cost > 0 && next_cost > 0)
+	{
+		if (big_cost < next_cost)
+			return (1);
+		if (big_cost > next_cost)
+			return (2);
+	}
+	else if (big_cost > 0 && next_cost < 0)
+	{
+		if (big_cost < (next_cost * -1))
+			return (1);
+		if (big_cost > (next_cost * -1))
+			return (2);
+	}
+	return (0);
+}
---
+++ final/srcs/cost/ft_calculate_cost.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calculate_cost.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 10:49:52 by baalbade          #+#    #+#             */
+/*   Updated: 2023/04/13 10:49:54 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_calculate_cost(t_list **stack, int node_idx)
+{
+	int	size;
+	int	cost;
+	int	mid;
+
+	size = ft_get_stack_size(stack);
+	mid = size / 2;
+	cost = 0;
+	if (node_idx == 1)
+		cost = 1;
+	if (node_idx == size)
+		cost = 1;
+	if (node_idx < mid)
+		cost = (size - (size - node_idx)) - 1;
+	if (node_idx > mid)
+		cost = ((size - node_idx) + 1) * -1 ;
+	return (cost);
+}
