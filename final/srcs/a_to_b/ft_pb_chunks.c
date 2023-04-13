6bda4a30ea051601df8b4258be8439c0ab1d7a64
Basil ALBADER <baalbade@student.42.fr>
2023-04-13 12:42:48 +0200

    clean up

--- final/srcs/a_to_b/ft_pb_chunks.c
+++ final/srcs/a_to_b/ft_pb_chunks.c
@@ -45,16 +45,9 @@ void	ft_pb_chunks(t_list **a, t_list **b)
 {
 	int	*pivots_arr;
 	int	pivots_count;
-	int	big;
-	int	next_big;
 
 	pivots_arr = ft_get_pivots(a);
 	pivots_count = 10;
 	ft_push_chunks_to_b(a, b, pivots_arr, pivots_count);
-	ft_re_init_index(*b);
-	ft_print_stack(b, "b");
-	big = ft_find_biggest_node_idx(b);
-	next_big = ft_find_next_biggest_node_idx(b);
-	cost = ft_big_or_next(big, next_big, b);
 	free(pivots_arr);
 }
