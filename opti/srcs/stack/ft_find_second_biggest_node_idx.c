0cd3426bb98d6e943e3ded6bc1bf7b6ee4d7277c
Basil ALBADER <baalbade@student.42.fr>
2023-04-15 13:16:58 +0200

    fct to find 2nd biggest node idx in given stack

---
+++ opti/srcs/stack/ft_find_second_biggest_node_idx.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_second_biggest_node_idx.c                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 12:48:36 by baalbade          #+#    #+#             */
+/*   Updated: 2023/04/15 12:48:38 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_find_second_biggest_node_idx(t_list **stack)
+{
+	t_list	*tmp;
+	int		second_idx;
+	int		second_data;
+	int		first_data;
+	int		i;
+
+	if (!stack)
+		return (0);
+	tmp = NULL;
+	tmp = *stack;
+	second_data = INT_MIN;
+	i = 0;
+	first_data = ft_find_biggest_node_data(stack);
+	while (tmp)
+	{
+		if (tmp->data > second_data && tmp->data < first_data)
+		{
+			second_data = tmp->data;
+			second_idx = tmp->index;
+		}
+		++i;
+		tmp = tmp->next;
+	}
+	return (second_idx);
+}
