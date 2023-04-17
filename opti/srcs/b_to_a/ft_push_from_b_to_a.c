527c898401e634fcfe178607be3e5069a63cb281
Basil ALBADER <baalbade@student.42.fr>
2023-04-17 14:27:56 +0200

    WIP

--- opti/srcs/b_to_a/ft_push_from_b_to_a.c
+++ opti/srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,26 +12,78 @@
 
 #include "push_swap.h"
 
-void	ft_push_nodes_to_a(t_list **a, t_list **b, int *indexes)
+static int *ft_get_sorted_idx_values(t_list **b,
+		int *sorted_indexes, int *sorted_values)
 {
+	t_list	*tmp;
+	t_list	*head;
+	int		i;
+	int		j;
 
+	tmp = (*b);
+	head = tmp;
+	i = 0;
+	j = 0;
+	while (tmp)
+	{
+		if (tmp->index == sorted_indexes[i])
+		{
+			sorted_values[j] = tmp->data;
+			tmp = head;
+			++i;
+			++j;
+		}
+		tmp = tmp->next;
+	}
+	return (sorted_values);
+}
+
+void	ft_push_nodes_to_a(t_list **a, t_list **b, int *sorted_indexes)
+{
+	// int	i;
+	int	*sorted_values;
+
+	sorted_values = (int *)malloc(sizeof(int) * 3);
+	if (!sorted_values)
+		return ;
+
+	ft_print_stack(b, "b");
+	// for (int j = 0; j < 3; ++j) {
+	// 	printf("sorted_indexes: [%d]\n", sorted_indexes[j]);
+	// }
+	sorted_values = ft_get_sorted_idx_values(b, sorted_indexes, sorted_values);
+	// for (int j = 0; j < 3; ++j) {
+	// 	printf("sorted_values: [%d]\n", sorted_values[j]);
+	// }
+	// i = 0;
+	// while (i < 3)
+	// {
+		ft_push_node_to_a(a, b, sorted_indexes[0]);
+		ft_push_node_to_a(a, b, sorted_indexes[1]);
+		ft_push_node_to_a(a, b, sorted_indexes[2]);
+		// ++i;
+	// }
 }
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
 	int	*sorted_indexes;
-	int	b_size;
-	int	i;
+	// int	b_size;
+	// int	i;
 
-	(void)a;
 	sorted_indexes = (int *)malloc(sizeof(int) * 3);
 	if (!sorted_indexes)
 		return ;
-	b_size = ft_get_stack_size(b);
-	i = 0;
-	while (i < (b_size / 3))
-	{
-		sorted_indexes = ft_compare_costs_and_sort_indexes(b, sorted_indexes);
-		++i;
-	}
+	// b_size = ft_get_stack_size(b);
+	// i = 0;
+	// while (i < (b_size / 3))
+	// {
+	// 	sorted_indexes = ft_compare_costs_and_sort_indexes(b, sorted_indexes);
+	// 	ft_push_nodes_to_a(a, b, sorted_indexes);
+	// 	++i;
+	// }
+	// ft_re_init_index(*a);
+	// ft_re_init_index(*b);
+	sorted_indexes = ft_compare_costs_and_sort_indexes(b, sorted_indexes);
+	ft_push_nodes_to_a(a, b, sorted_indexes);
 }
