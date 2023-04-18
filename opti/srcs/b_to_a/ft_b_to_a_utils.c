ec7d5451c2211d22d2f32e5b0ea8eba85fdeaa5c
Basil ALBADER <baalbade@student.42.fr>
2023-04-18 14:53:33 +0200

    WIP

--- opti/srcs/b_to_a/ft_b_to_a_utils.c
+++ opti/srcs/b_to_a/ft_b_to_a_utils.c
@@ -5,65 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/13 13:26:32 by baalbade          #+#    #+#             */
-/*   Updated: 2023/04/13 13:26:50 by baalbade         ###   ########.fr       */
+/*   Created: 2023/04/18 13:52:38 by baalbade          #+#    #+#             */
+/*   Updated: 2023/04/18 13:52:39 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 
-void	ft_check_cost_and_rotate(int cost, t_list **a, t_list **b)
-{
-	int	i;
-
-	if (cost == 0)
-		ft_pa(a, b);
-	if (cost > 0)
-	{
-		i = 0;
-		while (i < cost)
-		{
-			ft_rb(b);
-			++i;
-		}
-		ft_pa(a, b);
-	}
-	else if (cost < 0)
-	{
-		i = 0;
-		cost *= -1;
-		while (i < cost)
-		{
-			ft_rrb(b);
-			++i;
-		}
-		ft_pa(a, b);
-	}
-}
-
-void	ft_push_node_to_a(t_list **a, t_list **b, int node_idx)
-{
-	int	cost;
-
-	cost = ft_calculate_cost(b, node_idx);
-	ft_check_cost_and_rotate(cost, a, b);
-	ft_re_init_index(*a);
-	ft_re_init_index(*b);
-}
-
-int	ft_return_new_idx_to_push(t_list **b, int node_value)
-{
-	t_list	*tmp;
-	int		new_idx;
-
-	new_idx = 0;
-	ft_re_init_index(*b);
-	tmp = (*b);
-	while (tmp)
-	{
-		if (tmp->data == node_value)
-			new_idx = tmp->index;
-		tmp = tmp->next;
-	}
-	return (new_idx);
-}
--- opti/srcs/b_to_a/ft_push_from_b_to_a.c
+++ opti/srcs/b_to_a/ft_push_from_b_to_a.c
@@ -5,103 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/13 12:40:49 by baalbade          #+#    #+#             */
-/*   Updated: 2023/04/13 12:40:51 by baalbade         ###   ########.fr       */
+/*   Created: 2023/04/18 13:52:42 by baalbade          #+#    #+#             */
+/*   Updated: 2023/04/18 13:52:43 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	*ft_get_sorted_idx_values(t_list **b,
-		int *sorted_indexes, int *sorted_values)
+int	*ft_costs(int *cost_arr, t_list **b)
 {
-	t_list	*tmp;
-	t_list	*head;
-	int		i;
-	int		j;
+	int	cost_1st_big;
+	int	cost_2nd_big;
+	int	cost_3rd_big;
 
-	tmp = (*b);
-	head = tmp;
-	i = 0;
-	j = 0;
-	while (tmp)
-	{
-		if (tmp->index == sorted_indexes[i])
+	cost_arr[0]= ft_cost_first_big(b);
+	cost_arr[1]= ft_cost_second_big(b);
+	cost_arr[2]= ft_cost_third_big(b);
+	return (cost_arr);
+}
+
+void	ft_final_sort(t_list **a, t_list **b)
+{
+	int	*cost_arr;
+
+
+	cost_arr = (int *)malloc(sizeof(int) * 3);
+	if (!cost_arr)
+		return ;
+	cost_arr = ft_costs(b, cost_arr);
+
+	while ((*b))
 		{
-			sorted_values[j] = tmp->data;
-			tmp = head;
-			++i;
-			++j;
+			if (cost_arr[0] < cost_arr[1] && cost_arr[0] < cost_arr[2])
+				ft_
+
 		}
-		tmp = tmp->next;
-	}
-	return (sorted_values);
-}
-
-void	ft_push_nodes_to_a(t_list **a, t_list **b, int *sorted_values)
-{
-	int	first;
-	int	second;
-	int	third;
-
-	first = sorted_values[0];
-	second = sorted_values[1];
-	third = sorted_values[2];
-	// printf("first: %d\n", first);
-	// printf("second: %d\n", second);
-	// printf("third: %d\n", third);
-	if (first < second && first < third)
-		ft_if_smallest_is_first(a, b);
-	else if (first > second && first < third)
-		ft_if_middle_is_first(a, b);
-	else if (first > second && first > third)
-		ft_if_biggest_is_first(a, b);
-}
-
-void	ft_push_from_b_to_a(t_list **a, t_list **b)
-{
-	int	*sorted_indexes;
-	int	*sorted_values;
-	int	i;
-	int	a_size;
-
-	sorted_values = (int *)malloc(sizeof(int) * 3);
-	if (!sorted_values)
-		return ;
-	sorted_indexes = (int *)malloc(sizeof(int) * 3);
-	if (!sorted_indexes)
-		return ;
-	printf("\n");
-	sorted_indexes = ft_compare_costs_and_sort_indexes(b, sorted_indexes);
-	ft_print_stack(b, "b");
-	i = 0;
-	// while (i < 6)
-	while((*b) != NULL)
-	{
-		sorted_indexes = ft_compare_costs_and_sort_indexes(b, sorted_indexes);
-		sorted_values = ft_get_sorted_idx_values(b, sorted_indexes, sorted_values);
-		a_size = ft_get_stack_size(a);
-		ft_push_node_to_a(a, b, sorted_indexes[0]);
-		// if (a_size > 1)
-		// {
-		// 	// printf("a->data: %d\n", (*a)->data);
-		// 	// printf("a->next->data: %d\n", (*a)->next->data);
-		// 	if ((*a)->data > (*a)->next->data)
-		// 		ft_sa(a);
-		// }
-		ft_re_init_index(*a);
-		ft_push_nodes_to_a(a, b, sorted_values);
-		printf("--------------------------------------------> %d\n", a_size);
-		// if (a_size > 1)
-		// {
-		// 	// printf("a->data: %d\n", (*a)->data);
-		// 	// printf("a->next->data: %d\n", (*a)->next->data);
-		// 	if ((*a)->data > (*a)->next->data)
-		// 		ft_sa(a);
-		// }
-		ft_re_init_index(*a);
-		// ft_print_stack(a, "a");
-		// printf("\n");
-		++i;
-	}
 }
