ad70d6ad5e6bbf75518c9a6af6272958e81875b5
balbader <basilalbapro@gmail.com>
2023-04-24 19:02:32 +0200

    WIP

--- opti/srcs/b_to_a/ft_b_to_a_utils_2.c
+++ opti/srcs/b_to_a/ft_b_to_a_utils_2.c
@@ -12,6 +12,47 @@
 
 #include "push_swap.h"
 
-void	ft_push_first(t_list **b, int)
+void	ft_push_first(t_list **a, t_list **b, int first)
 {
+	int	cost;
+
+	ft_re_init_index(*b);
+	cost = ft_calculate_cost(b, first);
+	ft_check_cost_and_rotate(cost, a, b);
+}
+
+void	ft_push_second(t_list **a, t_list **b, int second, int first)
+{
+	int	cost;
+
+	ft_re_init_index(*b);
+	cost = ft_calculate_cost(b, second);
+	ft_check_cost_and_rotate(cost, a, b);
+	ft_re_init_index(*b);
+	first = ft_find_biggest_node_idx(b);
+	cost = ft_calculate_cost(b, first);
+	ft_check_cost_and_rotate(cost, a, b);
+	if ((*a)->data > (*a)->next->data)
+		ft_sa(a);
+}
+
+void	ft_push_third(t_list **a, t_list **b, int third, int first)
+{
+	int	cost;
+
+	ft_re_init_index(*b);
+	cost = ft_calculate_cost(b, third);
+	ft_check_cost_and_rotate(cost, a, b);
+	ft_re_init_index(*b);
+	first = ft_find_biggest_node_idx(b);
+	cost = ft_calculate_cost(b, first);
+	ft_check_cost_and_rotate(cost, a, b);
+	if ((*a)->data > (*a)->next->data)
+		ft_sa(a);
+	ft_re_init_index(*b);
+	first = ft_find_biggest_node_idx(b);
+	cost = ft_calculate_cost(b, first);
+	ft_check_cost_and_rotate(cost, a, b);
+	if ((*a)->data > (*a)->next->data)
+		ft_sa(a);
 }
--- opti/srcs/b_to_a/ft_push_from_b_to_a.c
+++ opti/srcs/b_to_a/ft_push_from_b_to_a.c
@@ -17,39 +17,25 @@ void		ft_push_node_to_a(t_list **a, t_list **b, int *data)
 	int	first;
 	int	second;
 	int	third;
-	int	cost;
 
 	first = ft_find_biggest_node_idx(b);
 	second = ft_find_second_biggest_node_idx(b);
 	third = ft_find_third_biggest_node_idx(b);
 	if (data[0] > data[1] && data[0] > data[2]) // big == cheapest
 	{
-		cost = ft_calculate_cost(b, first);
-		ft_check_cost_and_rotate(cost, a, b);
+		// printf("ft_push_first:\n");
+		ft_push_first(a, b, first);
 	}
 	if (data[0] < data[1] && data[0] > data[2]) // 2nd big == cheapest
 	{
-		cost = ft_calculate_cost(b, second);
-		ft_check_cost_and_rotate(cost, a, b);
-		ft_re_init_index(*b);
-		cost = ft_calculate_cost(b, first);
-		ft_check_cost_and_rotate(cost, a, b);
-		ft_sa(a);
+		// printf("ft_push_second:\n");
+		ft_push_second(a, b, second, first);
 	}
 	if (data[0] < data[1] && data[0] < data[2]) // 3rd big == cheapest
 	{
-		cost = ft_calculate_cost(b, third);
-		ft_check_cost_and_rotate(cost, a, b);
-		ft_re_init_index(*b);
-		cost = ft_calculate_cost(b, first);
-		ft_check_cost_and_rotate(cost, a, b);
-		ft_sa(a);
-		ft_re_init_index(*b);
-		cost = ft_calculate_cost(b, first);
-		ft_check_cost_and_rotate(cost, a, b);
-		ft_sa(a);
+		// printf("ft_push_third:\n");
+		ft_push_third(a, b, third, first);
 	}
-
 }
 
 int	*ft_costs(t_list **b, int *costs)
@@ -86,14 +72,21 @@ void	ft_final_sort(t_list **a, t_list **b)
 	}
 	printf("\n");
 	ft_push_node_to_a(a, b, data);
-	// ft_re_init_index(*b);
+	ft_re_init_index(*a);
+	ft_print_stack(a, "a");
 }
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
-	// (void)a;
-	ft_final_sort(a, b);
-	ft_print_stack(a, "a");
-	printf("\n");
-	ft_print_stack(b, "b");
+	int	i;
+
+	i = 0;
+	while (i < 20)
+	{
+		printf("-------------------------->%d\n", i + 1);
+		ft_re_init_index(*a);
+		ft_re_init_index(*b);
+		ft_final_sort(a, b);
+		++i;
+	}
 }
