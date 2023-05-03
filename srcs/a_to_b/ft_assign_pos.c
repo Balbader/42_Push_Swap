7a3f4bddd3208fe2863be44d1eab7f73c0d330c7
Basil ALBADER <baalbade@student.42.fr>
2023-05-03 13:48:12 +0200

    ft_assign_pos.c

--- srcs/a_to_b/ft_assign_pos.c
+++ srcs/a_to_b/ft_assign_pos.c
@@ -12,18 +12,54 @@
 
 # include "push_swap.h"
 
-static t_list	*ft_create_sorted(t_list **a, t_list *sorted)
+static void	ft_assign_pos_to_node(int *arr, t_list **a, int a_size)
 {
-	sorted = (*a);
-	ft_pre_sort_stack(sorted);
-	return (sorted);
+	t_list	*head;
+	int	i;
+	int	j;
+
+	head = (*a);
+	j = 1;
+	i = 0;
+	while (i < a_size)
+	{
+		while (*a)
+		{
+			if (arr[i] == (*a)->data)
+			{
+				(*a)->pos = j;
+				++j;
+			}
+			(*a) = (*a)->next;
+		}
+		++i;
+		(*a) = head;
+	}
+}
+
+static int	*ft_create_sorted_arr(t_list **a, int a_size, int *sorted_arr)
+{
+	sorted_arr = (int *)malloc(sizeof(int) * a_size);
+	if (!sorted_arr)
+		return (0);
+	sorted_arr = ft_copy_stack_to_arr(a, a_size, sorted_arr);
+	ft_sort_arr(sorted_arr, a_size);
+	return (sorted_arr);
 }
 
 void	ft_assign_pos(t_list **a)
 {
-	t_list	*sorted;
+	int	i;
+	int	a_size;
+	int	*sorted_arr;
 
-	sorted = NULL;
-	sorted = ft_create_sorted(a, sorted);
-	ft_print_stack(&sorted, "sorted");
+	a_size = ft_get_stack_size(a);
+	sorted_arr = NULL;
+	sorted_arr = ft_create_sorted_arr(a, a_size, sorted_arr);
+	i = 0;
+	while (i < a_size)
+	{
+		ft_assign_pos_to_node(sorted_arr, a, a_size);
+		++i;
+	}
 }
