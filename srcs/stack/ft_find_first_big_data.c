d8d91cbf0ded2f1743a25a84a6f176f0237c3e42
Basil ALBADER <baalbade@student.42.fr>
2023-05-03 09:25:18 +0200

    WIP

--- srcs/stack/ft_find_first_big_data.c
+++ srcs/stack/ft_find_first_big_data.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+int	ft_find_first_big_data(t_list **stack)
+{
+	t_list	*tmp;
+	int		big_data;
+
+	if (!stack)
+		return (0);
+	tmp = (*stack);
+	big_data = INT_MIN;
+	while (tmp)
+	{
+		if (tmp->data > big_data)
+			big_data = tmp->data;
+		tmp = tmp->next;
+	}
+	return (big_data);
+}
