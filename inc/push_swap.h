d7f37e3b2f624a31bdddab37b084f1107b4c31a3
Basil ALBADER <baalbade@student.42.fr>
2023-05-03 11:18:34 +0200

    WIP

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -23,7 +23,7 @@ typedef struct s_list
 {
 	int				data;
 	int				index;
-	int				final_pos;
+	int				pos;
 	struct s_list	*next;
 }				t_list;
 
@@ -88,7 +88,7 @@ void		ft_add_new_tail(t_list **lst, t_list *node);
 void		ft_free_stack(t_list **stack);
 void		ft_print_stack(t_list **stack, char *name);
 void		ft_re_init_index(t_list *stack);
-t_list		*ft_create_node(int data, int index);
+t_list		*ft_create_node(int data, int index, int pos);
 t_list		*ft_init_stack(t_list *stack_a, int *entries, int ac);
 t_list		*ft_get_before_last_node(t_list *stack);
 t_list		*ft_get_last_node(t_list *stack);
