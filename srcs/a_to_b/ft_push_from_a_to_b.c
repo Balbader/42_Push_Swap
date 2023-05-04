a4bc62639841d7bbf6dac02254a11936b52c880c
Basil ALBADER <baalbade@student.42.fr>
2023-05-04 07:08:21 +0200

    push big half to b except for the 3 biggest elements

--- srcs/a_to_b/ft_push_from_a_to_b.c
+++ srcs/a_to_b/ft_push_from_a_to_b.c
@@ -29,6 +29,29 @@ static void	ft_push_small_half(t_list **a, t_list **b, int a_size)
 	}
 }
 
+static void	ft_push_big_half(t_list **a, t_list **b, int a_size)
+{
+	int	first;
+	int	second;
+	int	third;
+	int	mid;
+	int	i;
+
+	first = ft_find_first_big_data(a);
+	second = ft_find_second_big_data(a);
+	third = ft_find_third_big_data(a);
+	mid = a_size / 2;
+	i = 0;
+	while (i < a_size)
+	{
+		if ((*a)->data == first || (*a)->data == second || (*a)->data == third)
+			ft_ra(a);
+		else
+			ft_pb(a, b);
+		++i;
+	}
+}
+
 void	ft_push_from_a_to_b(t_list **a, t_list **b)
 {
 	int		a_size;
@@ -36,4 +59,5 @@ void	ft_push_from_a_to_b(t_list **a, t_list **b)
 	ft_assign_pos(a);
 	a_size = ft_get_stack_size(a);
 	ft_push_small_half(a, b, a_size);
+	ft_push_big_half(a, b, a_size);
 }
