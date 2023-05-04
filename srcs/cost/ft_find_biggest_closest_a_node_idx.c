9189911e46c7e4e83b5b41cbd1325a49d8de6066
Basil ALBADER <baalbade@student.42.fr>
2023-05-04 09:24:27 +0200

    WIP

---
+++ srcs/cost/ft_find_biggest_closest_a_node_idx.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_biggest_closest_a_node_idx.c               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 09:03:29 by baalbade          #+#    #+#             */
+/*   Updated: 2023/05/04 09:03:30 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+ * Returns the index for the biggest a_node that is the closest ot the
+ * incoming b_node_pos;
+*/
+
+int	ft_find_biggest_closest_a_node_idx(t_list **a, int b_node_pos)
+{
+}
