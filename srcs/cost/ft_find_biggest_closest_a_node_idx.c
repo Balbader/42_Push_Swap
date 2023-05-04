2f1766975c1bc98b57fb7f5f9d0bb44cabf9e956
Basil ALBADER <baalbade@student.42.fr>
2023-05-04 10:39:42 +0200

    WIP

--- srcs/cost/ft_find_biggest_closest_a_node_idx.c
+++ srcs/cost/ft_find_biggest_closest_a_node_idx.c
@@ -17,6 +17,39 @@
  * incoming b_node_pos;
 */
 
-// int	ft_find_biggest_closest_a_node_idx(t_list **a, int b_node_pos)
-// {
-// }
+static int	ft_get_pos_to_find(int *a_pos, int a_size, int b_node_pos)
+{
+	int	pos_to_find;
+	int	closest_pos;
+	int	i;
+
+	pos_to_find = a_pos[0];
+	closest_pos = a_pos[0] - b_node_pos;
+	i = 1;
+	while (i < a_size)
+	{
+		if (a_pos[i] - b_node_pos < closest_pos)
+			pos_to_find = a_pos[i];
+		++i;
+	}
+	return (pos_to_find);
+}
+
+int	ft_find_biggest_closest_a_node_idx(t_list **a, t_list **b,
+									int *a_pos, int *b_pos)
+{
+	int	a_size;
+	int	b_size;
+	int	pos_to_find;
+	int	i;
+
+	a_size = ft_get_stack_size(a);
+	b_size = ft_get_stack_size(b);
+	i = 0;
+	while (i < b)
+		{
+			
+		}
+	pos_to_find = ft_get_pos_to_find(a_pos, a_size, b_node_pos);
+	printf("pos_to_find: %d\n", pos_to_find);
+}
