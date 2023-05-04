64cbe69033f2b93e103dfe7a66ef87b60b2ac199
Basil ALBADER <baalbade@student.42.fr>
2023-05-04 09:24:44 +0200

    ft_pos_a.c

---
+++ srcs/cost/ft_pos_a.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pos_a.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 09:15:48 by baalbade          #+#    #+#             */
+/*   Updated: 2023/05/04 09:15:49 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+ * Retruns an array containing the final position of each a_node
+ * in a sorted a_stack
+*/
+
+int	*ft_pos_a(t_list **a)
+{
+	t_list	*tmp;
+	int		*pos_a;
+	int		i;
+	int		a_size;
+
+	tmp = (*a);
+	a_size = ft_get_stack_size(a);
+	pos_a = (int *)malloc(sizeof(int) * a_size);
+	if (!pos_a)
+		return (0);
+	i = 0;
+	while (i < a_size)
+	{
+		pos_a[i] = tmp->pos;
+		++i;
+		tmp = tmp->next;
+	}
+	return (pos_a);
+}
