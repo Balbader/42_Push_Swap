c16f317cd7afe265c67a4978a33de4858f490db1
Basil ALBADER <baalbade@student.42.fr>
2023-05-05 11:19:24 +0200

    WIP

--- srcs/main.c
+++ srcs/main.c
@@ -63,13 +63,14 @@ int	main(int ac, char **av)
 	else
 	{
 		ft_push_from_a_to_b(&a, &b);
-		ft_get_total_cost_arr(&a, &b);
+		ft_init_tot_cost_arr(&a, &b);
+
 	}
-	ft_re_init_index(a);
-	ft_print_stack(&a, "a");
-	printf("\n");
-	ft_re_init_index(b);
-	ft_print_stack(&b, "b");
+	// ft_re_init_index(a);
+	// ft_print_stack(&a, "a");
+	// printf("\n");
+	// ft_re_init_index(b);
+	// ft_print_stack(&b, "b");
 	ft_free_stack(&a);
 	ft_free_stack(&b);
 	return (0);
