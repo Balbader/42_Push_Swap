6c0434b9e5b90f2c226c9a40874d62f689e19063
Basil ALBADER <baalbade@student.42.fr>
2023-05-05 11:19:21 +0200

    WIP

--- srcs/cost/ft_cost_utils.c
+++ srcs/cost/ft_cost_utils.c
@@ -12,4 +12,18 @@
 
 #include "push_swap.h"
 
+void	ft_init_tot_cost_arr(t_list **a, t_list **b)
+{
+	int	*tot_cost;
+	int	b_size;
 
+	b_size = ft_get_stack_size(b);
+	tot_cost = (int *)malloc(sizeof(int) * b_size);
+	if (!tot_cost)
+		return ;
+	tot_cost = ft_get_total_cost_arr(a, b, tot_cost);
+	for (int i = 0; i < b_size; ++i) {
+		printf("tot_cost[%d]: %d\n", i, tot_cost[i]);
+	}
+	free(tot_cost);
+}
