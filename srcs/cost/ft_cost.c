f9640aaf250443f37dadcdd041561ac619af091c
Basil ALBADER <baalbade@student.42.fr>
2023-05-06 15:53:45 +0200

    add data struct

--- srcs/cost/ft_cost.c
+++ srcs/cost/ft_cost.c
@@ -12,42 +12,39 @@
 
 #include "push_swap.h"
 
-
-int	*ft_get_a_final_cost_arr(t_list **a, t_list **b,
-							int *a_node_pos_to_ra, int *a_final_cost)
+typedef struct s_data
 {
 	t_list	*head;
 	int		a_size;
 	int		b_size;
 	int		i;
 	int		j;
+}				t_data;
 
-	a_size = ft_get_stack_size(a);
-	b_size = ft_get_stack_size(b);
-	head = (*a);
-	j = 0;
-	i = 0;
-	while (i < b_size)
+int	*ft_get_a_final_cost_arr(t_list **a, t_list **b,
+							int *a_node_pos_to_ra, int *a_final_cost)
+{
+	t_data	data;
+
+	data.a_size = ft_get_stack_size(a);
+	data.b_size = ft_get_stack_size(b) - 1;
+	data.head = (*a);
+	data.j = 0;
+	data.i = 0;
+	while (data.i < data.b_size)
 	{
 		while (*a)
 		{
-			if ((*a)->pos == a_node_pos_to_ra[i])
+			if ((*a)->pos && (*a)->pos == a_node_pos_to_ra[data.i])
 			{
-				printf("a->pos: %d\n", (*a)->pos);
-				printf("a_node_pos_to_ra[%d]: %d\n", i, a_node_pos_to_ra[i]);
-				a_final_cost[j] = ft_calculate_node_cost(a, (*a)->index, a_size);
-				printf("a_final_cost[%d]: %d\n", i, a_final_cost[i]);
-				printf("\n");
-				++i;
-				++j;
+				a_final_cost[data.j]
+						= ft_calculate_node_cost(a, (*a)->index, data.a_size);
+				++data.i;
+				++data.j;
 			}
 			(*a) = (*a)->next;
 		}
-		(*a) = head;
+		(*a) = data.head;
 	}
-	// ft_free_stack(&head);
-	// for (int i = 0; i < b_size; ++i) {
-	// 	printf("a_final_cost[%d]: %d\n", i, a_final_cost[i]);
-	// }
 	return (a_final_cost);
 }
