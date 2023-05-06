84c7ec31ec3176dbcd5ff9d6f7513292a37b9352
Basil ALBADER <baalbade@student.42.fr>
2023-05-06 16:22:10 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -19,8 +19,8 @@
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
 	int	*a_pos_arr;
-	// int	b_pos_arr[5] = {33, 21, 12, 14, 9};
-	int	*b_pos_arr;
+	int	b_pos_arr[5] = {23, 44, 12, 13, 32};
+	// int	*b_pos_arr;
 	int	*a_cost_arr;
 	int	*b_cost_arr;
 	int	b_size;
@@ -33,7 +33,7 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 	a_cost_arr = ft_get_cost_arr(a);
 
 	b_size = ft_get_stack_size(b);
-	b_pos_arr = ft_get_pos_arr(b);
+	// b_pos_arr = ft_get_pos_arr(b);
 	b_cost_arr = NULL;
 	b_cost_arr = ft_get_cost_arr(b);
 
@@ -47,8 +47,25 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 	if (!a_final_cost)
 		return ;
 	a_final_cost = ft_get_a_final_cost_arr(a, b, a_node_pos_to_ra, a_final_cost);
+
+	for (int i = 0; i < b_size; ++i) {
+		printf("a_final_cost[%d]: %d\n", i, a_final_cost[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < b_size; ++i) {
+		printf("b_cost_arr_cost[%d]: %d\n", i, b_cost_arr[i]);
+	}
+	printf("\n");
+
+	int	*final_cost;
+	final_cost = NULL;
+	final_cost = ft_get_tot_cost(a_final_cost, b_cost_arr, b_size);
+	for (int i = 0; i < b_size; ++i) {
+		printf("final_cost[%d]: %d\n", i, final_cost[i]);
+	}
+
 	free(a_pos_arr);
-	free(b_pos_arr);
+	// free(b_pos_arr);
 	free(a_cost_arr);
 	free(b_cost_arr);
 	free(a_node_pos_to_ra);
