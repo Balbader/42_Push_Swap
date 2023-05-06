4ad229d6fe5a0278f428eb20792a04d67e26f829
Basil ALBADER <baalbade@student.42.fr>
2023-05-06 12:33:44 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -27,34 +27,21 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 
 	a_size = ft_get_stack_size(a);
 	a_pos_arr = NULL;
-	a_pos_arr = ft_get_a_pos_arr(a);
-	for (int i = 0; i < a_size; ++i) {
-		printf("a_pos_arr[%d]: %d\n", i, a_pos_arr[i]);
-	}
-
-	printf("\n");
-
+	a_pos_arr = ft_get_pos_arr(a);
 	a_cost_arr = NULL;
 	a_cost_arr = ft_get_cost_arr(a);
-	for (int i = 0; i < a_size; ++i) {
-		printf("a_cost_arr[%d]: %d\n", i, a_cost_arr[i]);
-	}
-
-	printf("\n");
 
 	b_size = ft_get_stack_size(b);
-	b_pos_arr = NULL;
-	b_pos_arr = ft_get_b_pos_arr(b);
-	for (int i = 0; i < b_size; ++i) {
-		printf("b_pos_arr[%d]: %d\n", i, b_pos_arr[i]);
-	}
-
-	printf("\n");
-
+	b_pos_arr = ft_get_pos_arr(b);
 	b_cost_arr = NULL;
 	b_cost_arr = ft_get_cost_arr(b);
-	for (int i = 0; i < a_size; ++i) {
-		printf("a_cost_arr[%d]: %d\n", i, a_cost_arr[i]);
+
+	int *a_node_pos_to_ra;
+
+	a_node_pos_to_ra = ft_find_biggest_closest_a_node_idx(a, b, a_pos_arr, b_pos_arr);
+
+	for (int i = 0; i < b_size; ++i) {
+		printf("a_node_pos_to_ra[%d]: %d\n", i, a_node_pos_to_ra[i]);
 	}
 }
 
