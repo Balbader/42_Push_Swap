d11cda7e79bc4b0910e1dc6a9f9937f5a416954d
Basil ALBADER <baalbade@student.42.fr>
2023-05-08 07:45:03 +0200

    add ft_get_cheapest_idx , ft_cpy_a_fina_cost, ft_cheapest_idx, ft_reorder_stack functions

--- srcs/b_to_a/ft_push_from_b_to_a_utils.c
+++ srcs/b_to_a/ft_push_from_b_to_a_utils.c
@@ -12,13 +12,75 @@
 
 #include "push_swap.h"
 
-// int	ft_get_cheapest_idx(int *final_cost)
-// {
-// 	int	final_idx;
-// 	int	i;
+int	*ft_cpy_a_final_cost(int *a_final_cost, int a_final_cost_size, int *copy)
+{
+	int	i;
 
-// 	final_idx = INT_MIN;
-// 	i = 0;
-// 	while (i < )
-// 	return (final_idx);
-// }
+	copy = (int *)malloc(sizeof(int) * a_final_cost_size);
+	if (!copy)
+		return (0);
+	i = 0;
+	while (i < a_final_cost_size)
+	{
+		copy[i] = a_final_cost[i];
+		++i;
+	}
+	return (copy);
+}
+
+int	ft_get_cheapest_idx(int *final_cost, int b_size)
+{
+	int	final_idx;
+	int	i;
+
+	final_idx = INT_MAX;
+	i = 0;
+	while (i < b_size)
+	{
+		if (final_cost[i] < final_idx)
+			final_idx = i;
+		++i;
+	}
+	return (final_idx);
+}
+
+int	ft_get_hit(int *cost_arr, int cost_arr_size, int cheapest_idx)
+{
+	int	hit;
+	int	i;
+
+	i = 0;
+	while (i < cost_arr_size)
+	{
+		if (i == cheapest_idx)
+			hit = cost_arr[i];
+		++i;
+	}
+	return (hit);
+}
+
+void	ft_reorder_stack(t_list **stack, int hit)
+{
+	int	i;
+
+	if (hit == 0)
+		return ;
+	if (hit < 0)
+	{
+		i = hit;
+		while (i < 0)
+		{
+			ft_reverse_rotate(stack);
+			++i;
+		}
+	}
+	if (hit > 0)
+	{
+		i = 0;
+		while (i < hit)
+		{
+			ft_rotate(stack);
+			++i;
+		}
+	}
+}
