d482737469a3c0b6cd81bbc149d4e3bd4eafb931
balbader <basilalbapro@gmail.com>
2023-05-09 13:54:39 +0200

    WIP

--- srcs/b_to_a/ft_find_biggest_closest_a_node_idx.c
+++ srcs/b_to_a/ft_find_biggest_closest_a_node_idx.c
@@ -42,6 +42,12 @@ int	ft_get_pos_to_find(int *a_pos, int a_size, int b_node_pos)
 }
 
 /*
+ * ---------------->>>>>>   NOTE: ft_find_big_close_a_node_idx()
+ *  Need to change the return value from returning the position fo the a_node
+ *  that needs to be at the top of a_stack to returning its index
+*/
+
+/*
  * Returns an array containing the "pos" value of the a_node that needs
  * to be at the top of a_stack based on each b_node
 */
