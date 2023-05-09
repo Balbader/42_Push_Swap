5719c75e4db69b012f5fac94c085681de2b0ef2c
balbader <basilalbapro@gmail.com>
2023-05-09 10:47:53 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -14,6 +14,25 @@
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
-	(void)a;
-	(void)b;
+	int	*tot_cost_arr;
+	// int	a_size;
+	int	b_size;
+
+	// (void)b;
+	ft_re_init_index(*a);
+	ft_print_stack(a, "a");
+	printf("\n");
+	ft_re_init_index(*b);
+	ft_print_stack(b, "b");
+	printf("\n");
+	// a_size = ft_get_stack_size(a);
+	b_size = ft_get_stack_size(b);
+	tot_cost_arr = (int *)malloc(sizeof(int) * b_size);
+	if (!tot_cost_arr)
+		return ;
+	tot_cost_arr = ft_get_total_cost_arr(a, b, tot_cost_arr);
+	printf("\n");
+	for (int i = 0; i < b_size; ++i) {
+		printf("tot_cost_arr[%d]: %d\n", i, tot_cost_arr[i]);
+	}
 }
