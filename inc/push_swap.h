98d4eacd9f5929c7282020e51443853975539076
balbader <basilalbapro@gmail.com>
2023-05-11 11:36:13 +0200

    .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -44,12 +44,13 @@ t_list		*ft_pre_sort_stack(t_list *rand_stack);
 
 /* B_TO_A */
 int			ft_get_pos_to_find(int *a_pos, int a_size, int b_node_pos);
-int			ft_find_big_bro_idx(t_list **a, int incoming);
+int			ft_find_big_bro_idx(t_list **a, int incoming_pos);
 int			*ft_find_big_close_a_node_pos(t_list **a, t_list **b,
 									int *a_pos, int *b_pos);
 int			ft_find_cheapest_b_node_idx(t_list **a, t_list **b, int node_to_mv);
 int			ft_fetch_cheapest(int *tot_cost_arr, t_list **b);
-void		ft_reorder_a(t_list **a, int pos_to_find);
+void		ft_reorder_a(t_list **a, int big_bro_idx);
+void		ft_reorder_b(t_list **b, int small_bro_idx);
 void		ft_push_from_b_to_a(t_list **a, t_list **b);
 
 /* COST */
