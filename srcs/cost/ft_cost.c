4ed346ef710d84ccb3980c5d1a8f6f1f156e225a
balbader <basilalbapro@gmail.com>
2023-05-11 13:23:18 +0200

    ft_get_cost_arr.c

--- srcs/cost/ft_cost.c
+++ srcs/cost/ft_cost.c
@@ -10,26 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
 /*
  * Retruns an array with the cost of bringing each node to top of the stack
 */
-int	ft_get_cost_arr(t_list **stack, int *cost_arr)
+
+int	*ft_get_cost_arr(t_list **stack, int *cost_arr)
 {
 	int	stack_size;
 	int	mid;
 	int	i;
 
 	stack_size = ft_get_stack_size(stack);
+	cost_arr = (int *)malloc(sizeof(int) * stack_size);
+	if (!cost_arr)
+		return (0);
 	mid = stack_size / 2;
 	i = 0;
 	while (i < stack_size)
-		{
-			if (i <= mid)
-				cost_arr[i] = i;
-			else
-				cost_arr[i] = 
-			++i;
-		}
-
+	{
+		if (i <= mid)
+			cost_arr[i] = i;
+		else
+			cost_arr[i] = ((i - stack_size) + 1);
+		++i;
+	}
 	return (cost_arr);
 }
