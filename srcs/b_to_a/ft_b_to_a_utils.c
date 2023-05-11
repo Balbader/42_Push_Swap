1d1ce0a0265e6d409f39e994e2a13935e177df1d
balbader <basilalbapro@gmail.com>
2023-05-11 08:57:11 +0200

    add ft_reorder_a.c fct

--- srcs/b_to_a/ft_b_to_a_utils.c
+++ srcs/b_to_a/ft_b_to_a_utils.c
@@ -12,3 +12,41 @@
 
 #include "push_swap.h"
 
+static int	ft_check_a_pos(t_list **a, int big_bro_idx)
+{
+	int	a_size;
+	int	mid;
+
+	a_size = ft_get_stack_size(a);
+	mid = a_size / 2;
+	if (big_bro_idx < mid)
+		return (1);
+	return (0);
+}
+
+void	ft_reorder_a(t_list **a, int big_bro_idx)
+{
+	int	i;
+	int	rra_tot;
+	int	a_size;
+
+	a_size = ft_get_stack_size(a);
+	i = 0;
+	if (ft_check_a_pos(a, big_bro_idx) == 1)
+	{
+		while (i < big_bro_idx)
+		{
+			ft_ra(a);
+			++i;
+		}
+	}
+	else if (ft_check_a_pos(a, big_bro_idx) == 0)
+	{
+		rra_tot = a_size - (big_bro_idx - 1);
+		while (i < rra_tot)
+		{
+			ft_rra(a);
+			++i;
+		}
+	}
+}
