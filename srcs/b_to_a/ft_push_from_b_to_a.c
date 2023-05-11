fdce65893382a015b450ba2b0d993106f0e348e2
balbader <basilalbapro@gmail.com>
2023-05-11 08:57:14 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -15,7 +15,6 @@
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
 	int	*tot_cost_arr;
-	int	*a_final_cost_arr;
 	int	*a_pos_to_ra;
 	int	*a_pos;
 	int	*b_pos;
@@ -47,32 +46,29 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 	b_size = ft_get_stack_size(b);
 
 	tot_cost_arr = (int *)malloc(sizeof(int) * b_size);
-	a_final_cost_arr = (int *)malloc(sizeof(int) * b_size);
 	a_pos_to_ra = (int *)malloc(sizeof(int) * b_size);
 
-	if (!tot_cost_arr || !a_final_cost_arr || !a_pos_to_ra)
+	if (!tot_cost_arr || !a_pos_to_ra)
 		return ;
 
 	tot_cost_arr = ft_get_total_cost_arr(a, b, tot_cost_arr);
 	a_pos_to_ra = ft_find_big_close_a_node_pos(a, b, a_pos, b_pos);
-	a_final_cost_arr = ft_get_a_final_cost_arr(a, b, a_pos_to_ra, a_final_cost_arr);
 
 	printf("\n");
 
 	int	big_bro_idx;
 	big_bro_idx = 0;
 
+
 	for (int i = 0; i < b_size; ++i) {
 		// printf("a_cost[%d]: %d\n", i, a_cost[i]);
 		// printf("b_cost[%d]: %d\n", i, b_cost[i]);
 		// printf("tot_cost_arr[%d]: %d\n", i, tot_cost_arr[i]);
-		printf("a_pos_to_ra[%d]: %d\n", i, a_pos_to_ra[i]);
+		// printf("a_pos_to_ra[%d]: %d\n", i, a_pos_to_ra[i]);
 		big_bro_idx = ft_find_big_bro_idx(a, a_pos_to_ra[i]);
 		printf("big_bro_idx: %d\n", big_bro_idx);
-		// printf("a_final_cost_arr[%d]: %d\n", i, a_final_cost_arr[i]);
 		printf("\n");
 	}
-
 
 	free(a_pos);
 	free(b_pos);
