26b96e7745fcf558dc7eb6d843a01834ba7b39fd
balbader <basilalbapro@gmail.com>
2023-05-11 10:02:30 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,72 +12,41 @@
 
 #include "push_swap.h"
 
-void	ft_push_from_b_to_a(t_list **a, t_list **b)
+typedef struct s_data
 {
-	int	*tot_cost_arr;
-	int	*a_pos_to_ra;
 	int	*a_pos;
 	int	*b_pos;
-	// int	*a_cost;
-	// int	*b_cost;
-
-	// int	a_size;
+	int	*tot_cost_arr;
+	int	*a_pos_to_ra;
 	int	b_size;
+}				t_data;
 
-	// (void)b;
+static void	ft_free_data(int *d1, int *d2, int *d3, int *d4)
+{
+	free(d1);
+	free(d2);
+	free(d3);
+	free(d4);
+}
+
+void	ft_push_from_b_to_a(t_list **a, t_list **b)
+{
+	t_data	data;
+	int		cheap;
+
 	ft_re_init_index(*a);
-	ft_print_stack(a, "a");
-	printf("\n");
 	ft_re_init_index(*b);
-	ft_print_stack(b, "b");
-	printf("\n");
-
-	a_pos = NULL;
-	b_pos = NULL;
-	// a_cost = NULL;
-	// b_cost = NULL;
-
-	a_pos = ft_get_pos_arr(a);
-	b_pos = ft_get_pos_arr(b);
-	// a_cost = ft_get_cost_arr(a);
-	// b_cost = ft_get_cost_arr(b);
-
-	// a_size = ft_get_stack_size(a);
-	b_size = ft_get_stack_size(b);
-
-	tot_cost_arr = (int *)malloc(sizeof(int) * b_size);
-	a_pos_to_ra = (int *)malloc(sizeof(int) * b_size);
-
-	if (!tot_cost_arr || !a_pos_to_ra)
+	data.b_size = ft_get_stack_size(b);
+	data.a_pos = ft_get_pos_arr(a);
+	data.b_pos = ft_get_pos_arr(b);
+	data.tot_cost_arr = (int *)malloc(sizeof(int) * data.b_size);
+	data.a_pos_to_ra = (int *)malloc(sizeof(int) * data.b_size);
+	if (!data.tot_cost_arr || !data.a_pos_to_ra)
 		return ;
-
-	tot_cost_arr = ft_get_total_cost_arr(a, b, tot_cost_arr);
-	a_pos_to_ra = ft_find_big_close_a_node_pos(a, b, a_pos, b_pos);
-
-	printf("\n");
-
-	// int	big_bro_idx;
-	// big_bro_idx = 0;
-	// big_bro_idx = ft_find_big_bro_idx(a, a_pos_to_ra[i]);
-	// printf("big_bro_idx: %d\n", big_bro_idx);
-
-	for (int i = 0; i < b_size; ++i) {
-		printf("tot_cost_arr[%d]: %d\n", i, tot_cost_arr[i]);
-		printf("a_pos_to_ra[%d]: %d\n", i, a_pos_to_ra[i]);
-		printf("\n");
-	}
-
-	int	cheap;
-	cheap = 0;
-	cheap = ft_fetch_cheapest(tot_cost_arr, b);
+	data.tot_cost_arr = ft_get_total_cost_arr(a, b, data.tot_cost_arr);
+	data.a_pos_to_ra
+		= ft_find_big_close_a_node_pos(a, b, data.a_pos, data.b_pos);
+	cheap = ft_fetch_cheapest(data.tot_cost_arr, b);
 	printf("cheap: %d\n", cheap);
-
-	printf("a_pos_to_ra[%d]: %d\n", cheap, a_pos_to_ra[cheap]);
-	free(a_pos);
-	free(b_pos);
-	// free(a_cost);
-	// free(b_cost);
-	free(tot_cost_arr);
-	free(a_pos_to_ra);
-	// free(a_final_cost_arr);
+	ft_free_data(data.a_pos, data.b_pos, data.tot_cost_arr, data.a_pos_to_ra);
 }
