c8b06aaf9c044bbea8f7d27e79e50d08ed82d8e9
balbader <basilalbapro@gmail.com>
2023-05-12 14:04:24 +0200

    WIP

--- srcs/cost/ft_cost_utils_2.c
+++ srcs/cost/ft_cost_utils_2.c
@@ -12,11 +12,24 @@
 
 #include "push_swap.h"
 
+typedef struct s_data
+{
+	int	big_bro_pos;
+	int	big_bro_idx;
+	int	cheapest_lil_bro_pos;
+	int	cheapest_lil_bro_idx;
+	int	*a_pos;
+	int	*b_pos;
+	int	*a_cost;
+	int	*b_cost;
+	int	a_size;
+	int	b_size;
+}				t_data;
+
 /*
  * Returns an arr with the total cost of moving each b_node to a_stack
 */
-int	*ft_get_final_cost_arr(t_list **a, t_list **b)
+int	*ft_get_final_b_cost_arr(t_list **a, t_list **b)
 {
-	int	*cost_a;
-	int	*cost_b;
+	t_data	data;
 }
