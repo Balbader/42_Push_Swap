52974c605327aedba5f46302192d67ae730e0f58
Basil ALBADER <baalbade@student.42.fr>
2023-05-12 07:05:24 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -19,6 +19,7 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 	int	*pos_a;
 	int	*pos_b;
 	int	big_bro_idx;
+	int	big_bro_pos;
 
 	ft_re_init_index(*a);
 	ft_re_init_index(*b);
@@ -29,26 +30,29 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 	cost_b = NULL;
 	cost_a = ft_get_cost_arr(a, cost_a);
 	cost_b = ft_get_cost_arr(b, cost_b);
-	for (int i = 0; i < 3; ++i) {
-		printf("cost_a[%d]: %d\n", i, cost_a[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < 7; ++i) {
-		printf("cost_b[%d]: %d\n", i, cost_b[i]);
-	}
-	printf("\n");
+	// for (int i = 0; i < 3; ++i) {
+	// 	printf("cost_a[%d]: %d\n", i, cost_a[i]);
+	// }
+	// printf("\n");
+	// for (int i = 0; i < 7; ++i) {
+	// 	printf("cost_b[%d]: %d\n", i, cost_b[i]);
+	// }
+	// printf("\n");
 	pos_a = NULL;
 	pos_b = NULL;
 	pos_a = ft_get_pos_arr(a, pos_a);
 	pos_b = ft_get_pos_arr(b, pos_b);
-	for (int i = 0; i < 3; ++i) {
-		printf("pos_a[%d]: %d\n", i, pos_a[i]);
-	}
+	// for (int i = 0; i < 3; ++i) {
+	// 	printf("pos_a[%d]: %d\n", i, pos_a[i]);
+	// }
 	printf("\n");
 	for (int i = 0; i < 7; ++i) {
 		big_bro_idx = ft_get_big_bro_idx(a, pos_a, pos_b[i]);
+		big_bro_pos = ft_get_big_bro_pos(a, pos_a, pos_b[i]);
 		printf("big_bro_idx: %d\n", big_bro_idx);
+		printf("big_bro_pos: %d\n", big_bro_pos);
 		printf("pos_b[%d]: %d\n", i, pos_b[i]);
+		printf("\n");
 	}
 	free(cost_a);
 	free(cost_b);
