ef959989076c5e9085b0bbc4856c0d2365f426b6
balbader <basilalbapro@gmail.com>
2023-05-12 12:49:39 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a_opti.c
+++ srcs/b_to_a/ft_push_from_b_to_a_opti.c
@@ -24,6 +24,7 @@ int	ft_get_cheapest_lil_bro_idx(t_list **b, int lil_bro_idx)
 	int	cheapest;
 	int	i;
 
+	ft_re_init_index(*b);
 	b_size = ft_get_stack_size(b);
 	b_cost_arr = NULL;
 	b_cost_arr = ft_get_cost_arr(b, b_cost_arr);
@@ -41,4 +42,28 @@ int	ft_get_cheapest_lil_bro_idx(t_list **b, int lil_bro_idx)
 		++i;
 	}
 	return (free(b_cost_arr), lil_bro_idx);
+}
+
+
+int	ft_get_cheapest_lil_bro_pos(t_list **b, int lil_bro_pos)
+{
+	int	*b_pos_arr;
+	int	b_size;
+	int	lil_bro_idx;
+	int	i;
+
+	ft_re_init_index(*b);
+	b_size = ft_get_stack_size(b);
+	lil_bro_idx = 0;
+	lil_bro_idx = ft_get_cheapest_lil_bro_idx(b, lil_bro_idx);
+	b_pos_arr = NULL;
+	b_pos_arr = ft_get_pos_arr(b, b_pos_arr);
+	i = 0;
+	while (i < b_size)
+	{
+		if (i == lil_bro_idx)
+			lil_bro_pos = b_pos_arr[i];
+		++i;
+	}
+	return (free(b_pos_arr), lil_bro_pos);
 }
