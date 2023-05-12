6a03d6b0beeb801ad9135b533e3dd0be7027e5fa
Basil ALBADER <baalbade@student.42.fr>
2023-05-12 09:35:53 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,31 +12,34 @@
 
 #include "push_swap.h"
 
-static int	ft_get_incoming_idx(t_list **a, int big_bro_pos, int idx)
+void	ft_do_ra(t_list **a, int idx)
 {
-	t_list	*tmp;
-	int		a_size;
-	int		i;
+	int	i;
 
-	tmp = (*a);
-	a_size = ft_get_stack_size(a);
 	i = 0;
-	while (i < a_size)
+	while (i < idx)
 	{
-		if (tmp->pos == big_bro_pos)
-			idx = tmp->index;
-		tmp = tmp->next;
+		ft_ra(a);
 		++i;
 	}
-	ft_free_stack(&tmp);
-	return (idx);
+}
+
+void	ft_do_rra(t_list **a, int idx, int a_size)
+{
+	int	i;
+
+	i = 0;
+	while (i < (a_size - idx))
+	{
+		ft_rra(a);
+		++i;
+	}
 }
 
 static void	ft_rotate_a(t_list **a, int big_bro_pos)
 {
 	int	a_size;
 	int	mid;
-	int	i;
 	int	idx;
 
 	idx = 0;
@@ -44,35 +47,15 @@ static void	ft_rotate_a(t_list **a, int big_bro_pos)
 	a_size = ft_get_stack_size(a);
 	mid = a_size / 2;
 	if (idx <= mid)
-	{
-		i = 0;
-		while (i < idx)
-		{
-			ft_ra(a);
-			++i;
-		}
-	}
+		ft_do_ra(a, idx);
 	else if (idx > mid)
-	{
-		i = 0;
-		while (i < (a_size - idx))
-		{
-			ft_rra(a);
-			++i;
-		}
-	}
+		ft_do_rra(a, idx, a_size);
 }
 
 static void	ft_free_pos(int *pos_a, int *pos_b)
 {
 	free(pos_a);
 	free(pos_b);
-}
-
-static void	ft_re_init_stacks_index(t_list **a, t_list **b)
-{
-	ft_re_init_index(*a);
-	ft_re_init_index(*b);
 }
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
@@ -92,11 +75,11 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 		ft_re_init_stacks_index(a, b);
 		pos_a = ft_get_pos_arr(a, pos_a);
 		pos_b = ft_get_pos_arr(b, pos_b);
-		printf("pos_b[0]: %d\n", pos_b[0]);
 		big_bro_pos = ft_get_big_bro_pos(a, pos_a, pos_b[0]);
 		ft_rotate_a(a, big_bro_pos);
 		ft_pa(a, b);
 		ft_free_pos(pos_a, pos_b);
 		++i;
 	}
+	ft_reorder_a(a);
 }
