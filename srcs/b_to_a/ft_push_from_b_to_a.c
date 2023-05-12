53ee44cd2e19ac50dd3601d0831d0f0676d513ba
Basil ALBADER <baalbade@student.42.fr>
2023-05-12 08:02:13 +0200

    add ft_reorder_a() fct

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,51 +12,51 @@
 
 #include "push_swap.h"
 
+static void	ft_reorder_a(t_list **a, int big_bro_idx)
+{
+	int	a_size;
+	int	mid;
+	int	i;
+
+	a_size = ft_get_stack_size(a);
+	mid = a_size / 2;
+	i = 0;
+	if (big_bro_idx <= mid)
+	{
+		while (i < big_bro_idx)
+		{
+			ft_ra(a);
+			++i;
+		}
+	}
+	else
+	{
+		while (i < (a_size - big_bro_idx))
+		{
+			ft_rra(a);
+			++i;
+		}
+	}
+}
+
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
-	int	*cost_a;
-	int	*cost_b;
 	int	*pos_a;
-	int	pos_b[3] = {16, 14, 15};
-	// int	*pos_b;
+	int	*pos_b;
 	int	big_bro_idx;
 	int	big_bro_pos;
 
-	ft_re_init_index(*a);
-	ft_re_init_index(*b);
-	ft_print_stack(b, "b");
-	ft_print_stack(a, "a");
-	printf("\n");
-	cost_a = NULL;
-	cost_b = NULL;
-	cost_a = ft_get_cost_arr(a, cost_a);
-	cost_b = ft_get_cost_arr(b, cost_b);
-	// for (int i = 0; i < 3; ++i) {
-	// 	printf("cost_a[%d]: %d\n", i, cost_a[i]);
-	// }
-	// printf("\n");
-	// for (int i = 0; i < 7; ++i) {
-	// 	printf("cost_b[%d]: %d\n", i, cost_b[i]);
-	// }
+	// ft_re_init_index(*a);
+	// ft_re_init_index(*b);
+	// ft_print_stack(b, "b");
+	// ft_print_stack(a, "a");
 	// printf("\n");
 	pos_a = NULL;
-	// pos_b = NULL;
+	pos_b = NULL;
 	pos_a = ft_get_pos_arr(a, pos_a);
-	// pos_b = ft_get_pos_arr(b, pos_b);
-	// for (int i = 0; i < 3; ++i) {
-	// 	printf("pos_a[%d]: %d\n", i, pos_a[i]);
-	// }
-	printf("\n");
-	for (int i = 0; i < 3; ++i) {
-		big_bro_idx = ft_get_big_bro_idx(a, pos_a, pos_b[i]);
-		big_bro_pos = ft_get_big_bro_pos(a, pos_a, pos_b[i]);
-		printf("big_bro_idx: %d\n", big_bro_idx);
-		printf("big_bro_pos: %d\n", big_bro_pos);
-		printf("pos_b[%d]: %d\n", i, pos_b[i]);
-		printf("\n");
-	}
+	pos_b = ft_get_pos_arr(b, pos_b);
 	free(cost_a);
 	free(cost_b);
 	free(pos_a);
-	// free(pos_b);
+	free(pos_b);
 }
