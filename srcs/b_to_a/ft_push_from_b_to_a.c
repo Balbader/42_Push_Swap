4cbd7b95ff262d14da88c01ddee359e24e4998db
Basil ALBADER <baalbade@student.42.fr>
2023-05-12 08:38:34 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,26 +12,51 @@
 
 #include "push_swap.h"
 
-static void	ft_reorder_a(t_list **a, int big_bro_idx)
+static int	ft_get_incoming_idx(t_list **a, int	big_bro_pos, int idx)
+{
+	t_list	*tmp;
+	int		a_size;
+	int		i;
+
+	tmp = (*a);
+	a_size = ft_get_stack_size(a);
+	i = 0;
+	while (i < a_size)
+	{
+		if (tmp->pos == big_bro_pos)
+			idx = tmp->index;
+		tmp = tmp->next;
+		++i;
+	}
+	ft_free_stack(&tmp);
+	return (idx);
+}
+
+static void	ft_reorder_a(t_list **a, int big_bro_pos)
 {
 	int	a_size;
 	int	mid;
 	int	i;
+	int	idx;
 
+	idx = 0;
+	idx = ft_get_incoming_idx(a, big_bro_pos, idx);
+	printf("incomin_idx: %d\n", idx);
 	a_size = ft_get_stack_size(a);
 	mid = a_size / 2;
-	i = 0;
-	if (big_bro_idx <= mid)
+	if (idx <= mid)
 	{
-		while (i < big_bro_idx)
+		i = 0;
+		while (i < idx)
 		{
 			ft_ra(a);
 			++i;
 		}
 	}
-	else
+	else if (idx > mid)
 	{
-		while (i < (a_size - big_bro_idx))
+		i = 0;
+		while (i < (a_size - idx))
 		{
 			ft_rra(a);
 			++i;
@@ -43,20 +68,37 @@ void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
 	int	*pos_a;
 	int	*pos_b;
-	int	big_bro_idx;
 	int	big_bro_pos;
+	int	i;
+	int b_size;
 
-	// ft_re_init_index(*a);
-	// ft_re_init_index(*b);
+	ft_re_init_index(*a);
+	ft_re_init_index(*b);
 	// ft_print_stack(b, "b");
 	// ft_print_stack(a, "a");
 	// printf("\n");
 	pos_a = NULL;
 	pos_b = NULL;
-	pos_a = ft_get_pos_arr(a, pos_a);
-	pos_b = ft_get_pos_arr(b, pos_b);
-	free(cost_a);
-	free(cost_b);
+	// printf("big_bro_idx: %d\n", big_bro_idx);
+	b_size = ft_get_stack_size(b);
+	i = 0;
+	while (i < 2)
+	{
+		printf("---------------------> %d\n", i + 1);
+		ft_re_init_index(*a);
+		ft_re_init_index(*b);
+		b_size = ft_get_stack_size(b);
+		pos_a = ft_get_pos_arr(a, pos_a);
+		pos_b = ft_get_pos_arr(b, pos_b);
+		big_bro_pos = ft_get_big_bro_pos(a, pos_a, pos_b[i]);
+		ft_print_stack(b, "b");
+		ft_print_stack(a, "a");
+		printf("pos_b[%d]: %d\n", i, pos_b[i]);
+		printf("big_bro_pos: %d\n", big_bro_pos);
+		ft_reorder_a(a, big_bro_pos);
+		ft_pa(a, b);
+		++i;
+	}
 	free(pos_a);
 	free(pos_b);
 }
