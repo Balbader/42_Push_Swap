f63a91844d0db38663808ecd0345a9a13cfe67b2
Basil ALBADER <baalbade@student.42.fr>
2023-05-12 12:27:14 +0200

    ft_get_cheapest_lil_bro_idx() fct

--- srcs/b_to_a/ft_push_from_b_to_a_opti.c
+++ srcs/b_to_a/ft_push_from_b_to_a_opti.c
@@ -12,3 +12,33 @@
 
 #include "push_swap.h"
 
+/*
+ * What am I trying to do?
+ * Get the cheapest a_node_idx (big_bro_idx) && b_node_idx (lil_bro_idx)
+*/
+
+int	ft_get_cheapest_lil_bro_idx(t_list **b, int lil_bro_idx)
+{
+	int	*b_cost_arr;
+	int	b_size;
+	int	cheapest;
+	int	i;
+
+	b_size = ft_get_stack_size(b);
+	b_cost_arr = NULL;
+	b_cost_arr = ft_get_cost_arr(b, b_cost_arr);
+	cheapest = INT_MAX;
+	i = 0;
+	while (i < b_size)
+	{
+		if (b_cost_arr[i] < 0)
+			b_cost_arr[i] *= -1;
+		if (b_cost_arr[i] < cheapest )
+		{
+			cheapest = b_cost_arr[i];
+			lil_bro_idx = i;
+		}
+		++i;
+	}
+	return (free(b_cost_arr), lil_bro_idx);
+}
