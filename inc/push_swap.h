b167c77ada7a4f37b35261d73a846cc24145ff39
Basil ALBADER <baalbade@student.42.fr>
2023-05-13 15:18:21 +0200

    .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -51,8 +51,7 @@ void		ft_do_rra(t_list **a, int idx, int a_size);
 void		ft_reorder_a(t_list **a);
 
 /* COST */
-int			*ft_get_cost_arr(t_list **stack, int *cost_arr);
-int			*ft_get_pos_arr(t_list **stack, int *pos_arr);
+int			ft_get_node_cost(t_list **stack, int idx);
 int			ft_get_big_bro_idx(t_list **a, int *a_pos, int incoming_pos);
 int			ft_get_big_bro_pos(t_list **a, int *a_pos, int incoming_pos);
 int			ft_get_cheapest_lil_bro_idx(t_list **b, int lil_bro_idx);
