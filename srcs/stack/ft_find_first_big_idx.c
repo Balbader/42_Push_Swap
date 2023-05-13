97d9754836fdd691b6daa068dfbffb3711fc1a25
Basil ALBADER <baalbade@student.42.fr>
2023-05-13 14:31:28 +0200

    WIP

--- srcs/stack/ft_find_first_big_idx.c
+++ srcs/stack/ft_find_first_big_idx.c
@@ -12,27 +12,23 @@
 
 #include "push_swap.h"
 
-int	ft_find_first_big_idx(t_list **stack)
+int	ft_find_first_big_idx(t_list *stack)
 {
-	t_list	*tmp;
 	int		data;
 	int		index;
 
 	if (!stack)
 		return (0);
-	tmp = NULL;
-	tmp = (*stack);
-	data = tmp->data;
-	index = tmp->index;
-	while (tmp)
+	data = stack->data;
+	index = stack->index;
+	while (stack)
 	{
-		if (tmp->data > data)
+		if (stack->data > data)
 		{
-			data = tmp->data;
-			index = tmp->index;
+			data = stack->data;
+			index = stack->index;
 		}
-		tmp = tmp->next;
+		stack = stack->next;
 	}
-	ft_free_stack(&tmp);
 	return (index);
 }
