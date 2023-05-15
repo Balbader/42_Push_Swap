e35148850a17093c1248f01339c0f84261df92c5
Basil ALBADER <baalbade@student.42.fr>
2023-05-15 13:31:17 +0200

    init operations strin 2d array

--- srcs/b_to_a/ft_push_from_b_to_a_utils.c
+++ srcs/b_to_a/ft_push_from_b_to_a_utils.c
@@ -11,3 +11,18 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_init_operations_arr(char **op_arr, char *str)
+{
+	int	i;
+
+	(*op_arr)++ = (char *)malloc(sizeof(char) * ft_strlen(str));
+	if (!(*op_arr))
+		return ;
+	i = 0;
+	while (i < ft_strlen(str))
+	{
+		*op_arr[i] = str[i];
+		++i;
+	}
+}
