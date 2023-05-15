46609a29df4cdca9d618fefdd0add77729a643a6
Basil ALBADER <baalbade@student.42.fr>
2023-05-15 16:09:00 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a_utils.c
+++ srcs/b_to_a/ft_push_from_b_to_a_utils.c
@@ -12,44 +12,13 @@
 
 #include "push_swap.h"
 
-void	ft_free_arr(char **arr)
-{
-	int	arr_size;
-	int	i;
+// t_cmds	*ft_initial_op_arr(t_cmds *op_lst)
+// {
+// }
 
-	arr_size = sizeof(**arr) / sizeof(*arr[0]);
-	printf("arr_size: %d\n", arr_size);
-	i = 0;
-	while (i < arr_size)
-	{
-		free(arr[i]);
-		++i;
-	}
-}
-char	**ft_initial_op_arr(char **op_arr)
+void	ft_init_op_lst(t_cmds *op_lst, char *str)
 {
-	op_arr = (char **)malloc(sizeof(char *) * 10000);
-	if (!op_arr)
-		return (NULL);
-	return (op_arr);
-}
-
-void	ft_init_op_arr(char **op_arr, char *str, int i)
-{
-	int		j;
-
-	op_arr = ft_initial_op_arr(op_arr);
-	op_arr[i] = (char *)malloc(sizeof(char) * ft_strlen(&str[i]));
-	printf("\nstr[i] len: %d\n", ft_strlen(&str[i]));
-	if (!op_arr)
-		return ;
-	j = 0;
-	while (j < ft_strlen(&str[i]))
-	{
-		op_arr[j] = &str[j];
-		++j;
-	}
-	op_arr[j] = "\0";
-	printf("%s ", op_arr[i]);
-	printf("\n");
+	op_lst = NULL;
+	op_lst = ft_create_cmd_node(str);
+	ft_free_op_lst(&op_lst);
 }
