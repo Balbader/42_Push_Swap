f822b1858a790b1bb62c2e925bc0d01b4c1eb02d
Basil ALBADER <baalbade@student.42.fr>
2023-05-15 09:04:18 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -12,7 +12,6 @@
 
 #include "push_swap.h"
 
-/*
 void	ft_do_ra(t_list **a, int idx)
 {
 	int	i;
@@ -37,71 +36,51 @@ void	ft_do_rra(t_list **a, int idx, int a_size)
 	}
 }
 
-static void	ft_rotate_a(t_list **a, int big_bro_pos)
+static void	ft_rotate_a(t_list **a, int	big_bro_idx)
 {
 	int	a_size;
 	int	mid;
-	int	idx;
 
-	idx = 0;
-	idx = ft_get_incoming_idx(a, big_bro_pos, idx);
 	a_size = ft_get_stack_size(a);
 	mid = a_size / 2;
-	if (idx <= mid)
-		ft_do_ra(a, idx);
-	else if (idx > mid)
-		ft_do_rra(a, idx, a_size);
+	if (big_bro_idx <= mid)
+		ft_do_ra(a, big_bro_idx);
+	else if (big_bro_idx > mid)
+		ft_do_rra(a, big_bro_idx, a_size);
 }
 
-static void	ft_free_pos(int *pos_a, int *pos_b)
+static void	ft_reorder_a(t_list **a)
 {
-	free(pos_a);
-	free(pos_b);
+	int	smallest_idx;
+	int	a_size;
+	int	mid;
+
+	ft_re_init_index(*a);
+	smallest_idx = ft_find_smallest_node_idx(*a);
+	a_size = ft_get_stack_size(a);
+	mid = a_size / 2;
+	if (smallest_idx <= mid)
+		ft_do_ra(a, smallest_idx);
+	else if (smallest_idx > mid)
+		ft_do_rra(a, smallest_idx, a_size);
 }
-*/
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
-	int	cheapest_idx;
-	cheapest_idx = ft_get_cheapest_b_node_idx(a, b);
-	printf("cheapest_idx: %d\n", cheapest_idx);
+	int	lil_bro_pos;
+	int	big_bro_idx;
+	int	big_bro_pos;
+	int	b_size;
+
+	lil_bro_pos = 0;
+	while (*b)
+		{
+			b_size = ft_get_stack_size(b);
+			lil_bro_pos = ft_get_lil_bro_pos(a, b, lil_bro_pos);
+			big_bro_idx = ft_get_big_bro_idx(a, lil_bro_pos);
+			big_bro_pos = ft_get_big_bro_pos(a, big_bro_idx);
+			ft_rotate_a(a, big_bro_idx);
+			ft_pa(a, b);
+		}
+	ft_reorder_a(a);
 }
-
-/*
-	* find cheapest (b_cost + a_cost)
-	* Get big_bro_pos based on cheapest
-	* ft_rotate_a(a, big_bro_pos);
-	* Get lil_bro_pos based on cheapest
-	* ft_rotate_b(b, lil_bro_pos);
-	* ft_pa
-*/
-// void	ft_push_from_b_to_a(t_list **a, t_list **b)
-// {
-// 	int	*final_b_cost_arr;
-// 	int	cheapest_idx;
-// 	int	*pos_a;
-// 	int	*pos_b;
-// 	int	big_bro_pos;
-// 	int	b_size;
-
-// 	cheapest_idx = 0;
-// 	final_b_cost_arr = NULL;
-// 	pos_a = NULL;
-// 	pos_b = NULL;
-// 	b_size = ft_get_stack_size(b);
-// 	while (*b)
-// 	{
-// 		ft_re_init_stacks_index(a, b);
-// 		b_size = ft_get_stack_size(b);
-// 		final_b_cost_arr = ft_get_final_b_cost_arr(a, b, final_b_cost_arr);
-// 		cheapest_idx = ft_get_final_cheapest_idx(final_b_cost_arr, cheapest_idx,
-// 				b_size);
-// 		pos_a = ft_get_pos_arr(a, pos_a);
-// 		pos_b = ft_get_pos_arr(b, pos_b);
-// 		big_bro_pos = ft_get_big_bro_pos(a, pos_a, pos_b[cheapest_idx]);
-// 		ft_rotate_a(a, big_bro_pos);
-// 		ft_pa(a, b);
-// 		ft_free_pos(pos_a, pos_b);
-// 	}
-// 	ft_reorder_a(a);
-// }
