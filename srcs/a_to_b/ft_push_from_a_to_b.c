2c1d87d4002aae8ffe6e54db0695c83e7f05bace
Basil ALBADER <baalbade@student.42.fr>
2023-05-15 11:29:48 +0200

    WIP

--- srcs/a_to_b/ft_push_from_a_to_b.c
+++ srcs/a_to_b/ft_push_from_a_to_b.c
@@ -17,6 +17,7 @@ typedef struct s_data
 	int	first;
 	int	second;
 	int	third;
+	int	a_size;
 	int	i;
 	int	mid;
 	int	top_pivot;
@@ -39,10 +40,19 @@ static void	ft_init_data(t_list *a, t_data *data, int a_size)
 	data->btm_count = data->mid;
 }
 
-static void ft_check_b_pos(int mid, t_list **b)
+static void	ft_push_b_and_check_pos(int mid, t_list **a, t_list **b)
 {
+	ft_pb(a, b);
 	if ((*b)->pos < mid)
 		ft_rb(b);
+}
+
+static void	ft_check_condition(t_data *data)
+{
+	if (data->top_count == data->top_pivot)
+		data->top_pivot += data->pivot;
+	if (data->btm_count == data->btm_pivot)
+		data->btm_pivot -= data->pivot;
 }
 
 static void	ft_push_chunks_to_b(t_list **a, t_list **b, int a_size)
@@ -55,58 +65,22 @@ static void	ft_push_chunks_to_b(t_list **a, t_list **b, int a_size)
 		if ((*a)->data == data.first
 			|| (*a)->data == data.second || (*a)->data == data.third)
 			ft_ra(a);
-		else
+		ft_check_condition(&data);
+		if ((*a)->pos <= data.top_pivot && (*a)->pos >= data.mid)
 		{
-			if (data.top_count == data.top_pivot)
-				data.top_pivot += data.pivot;
-			if (data.btm_count == data.btm_pivot)
-				data.btm_pivot -= data.pivot;
-			if ((*a)->pos <= data.top_pivot && (*a)->pos >= data.mid)
-			{
-				ft_pb(a, b);
-				ft_check_b_pos(data.mid, b);
-				--a_size;
-				++data.top_count;
-			}
-			if ((*a)->pos >= data.btm_pivot && (*a)->pos < data.mid)
-			{
-				ft_pb(a, b);
-				ft_check_b_pos(data.mid, b);
-				--a_size;
-				--data.btm_count;
-			}
-			ft_ra(a);
+			ft_push_b_and_check_pos(data.mid, a, b);
+			--a_size;
+			++data.top_count;
 		}
+		if ((*a)->pos >= data.btm_pivot && (*a)->pos < data.mid)
+		{
+			ft_push_b_and_check_pos(data.mid, a, b);
+			--a_size;
+			--data.btm_count;
+		}
+		ft_ra(a);
 	}
 }
-
-// static void	ft_push_chunks_to_b(t_list **a, t_list **b, int a_size)
-// {
-// 	t_data	data;
-
-// 	ft_get_big_trio(*a, &data);
-// 	data.pivot = ft_define_pivot(a, data.pivot);
-// 	data.new_pivot = data.pivot;
-// 	data.count = 0;
-// 	while ((*a) && a_size > 3)
-// 	{
-// 		if ((*a)->data == data.first
-// 			|| (*a)->data == data.second || (*a)->data == data.third)
-// 			ft_ra(a);
-// 		else
-// 		{
-// 			if (data.count == data.new_pivot)
-// 				data.new_pivot += data.pivot;
-// 			if ((*a)->pos <= data.new_pivot)
-// 			{
-// 				ft_pb(a, b);
-// 				--a_size;
-// 				++data.count;
-// 			}
-// 			ft_ra(a);
-// 		}
-// 	}
-// }
 
 void	ft_push_from_a_to_b(t_list **a, t_list **b)
 {
