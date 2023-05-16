b61a55e1946368307fee490bf56272b0f6cd3e3d
Basil ALBADER <baalbade@student.42.fr>
2023-05-16 14:06:03 +0200

    WIP

--- srcs/b_to_a/ft_push_from_b_to_a.c
+++ srcs/b_to_a/ft_push_from_b_to_a.c
@@ -49,6 +49,7 @@ void	ft_rotate_a(t_list **a, int big_bro_idx)
 		ft_do_rra(a, big_bro_idx, a_size);
 }
 
+/*
 static void	ft_push_node_to_a_and_check_next(t_list **a, t_list **b)
 {
 	int	*big_3_idx;
@@ -72,14 +73,32 @@ static void	ft_push_node_to_a_and_check_next(t_list **a, t_list **b)
 	free(big_3_idx);
 	free(big_3_pos);
 }
+*/
+
+// void	ft_push_from_b_to_a(t_list **a, t_list **b)
+// {
+// 	while (*b)
+// 	{
+// 		ft_re_init_index(*a);
+// 		ft_re_init_index(*b);
+// 		ft_push_node_to_a_and_check_next(a, b);
+// 	}
+// 	ft_reorder_a(a);
+// }
 
 void	ft_push_from_b_to_a(t_list **a, t_list **b)
 {
+	int	lil_bro_idx;
+	int	lil_bro_pos;
+	int	big_bro_idx;
+
 	while (*b)
-	{
-		ft_re_init_index(*a);
-		ft_re_init_index(*b);
-		ft_push_node_to_a_and_check_next(a, b);
-	}
-	ft_reorder_a(a);
+		{
+			lil_bro_idx = ft_get_lil_bro_idx(b, lil_bro_idx);
+			lil_bro_pos = ft_get_lil_bro_pos(a, b, lil_bro_pos);
+			big_bro_idx = ft_get_big_bro_idx(a, lil_bro_pos);
+			ft_rotate_a(a, big_bro_idx);
+			ft_pa(a, b);
+		}
+		ft_reorder_a(a);
 }
