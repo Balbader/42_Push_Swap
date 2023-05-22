eafa56844522fac7c629fcb12f850287b8cb33de
balbader <basilalbapro@gmail.com>
2023-05-22 07:09:34 +0200

    ft_rra_rrb.c

---
+++ srcs/combos/ft_rra_rrb.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rra_rrb.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 06:48:12 by baalbade          #+#    #+#             */
+/*   Updated: 2023/05/22 06:48:17 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_do_rra_rrb(t_list **a, t_list **b, int a_idx, int b_idx)
+{
+	int	a_size;
+	int	b_size;
+
+	a_size = ft_get_stack_size(a);
+	b_size = ft_get_stack_size(b);
+	while ((a_idx && a_idx <= a_size) && (b_idx && b_idx <= b_size))
+	{
+		ft_rrr(a, b);
+		++a_idx;
+		++b_idx;
+	}
+	while (a_idx && a_idx <= a_size)
+	{
+		ft_rra(a);
+		++a_idx;
+	}
+	while (b_idx && b_idx <= b_size)
+	{
+		ft_rrb(b);
+		++b_idx;
+	}
+	ft_pa(a, b);
+}
