a0dac3a889f9bb52e6588d60bc352db8c30b7973
balbader <basilalbapro@gmail.com>
2023-05-22 06:41:56 +0200

    .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -47,6 +47,19 @@ int			ft_get_lil_bro_idx(t_list **b, int lil_bro_idx);
 int			ft_get_cheapest_b_node_idx(t_list **a, t_list **b);
 
 /* OPERATIONS */
+void		ft_pa(t_list **stack_b, t_list **stack_a);
+void		ft_pb(t_list **stack_a, t_list **stack_b);
+void		ft_ra(t_list **stack_a);
+void		ft_rb(t_list **stack_b);
+void		ft_rr(t_list **stack_a, t_list **stack_b);
+void		ft_rra(t_list **stack_a);
+void		ft_rrb(t_list **stack_b);
+void		ft_rrr(t_list **stack_a, t_list **stack_b);
+void		ft_sa(t_list *stack_a);
+void		ft_sb(t_list *stack_b);
+void		ft_ss(t_list *stack_a, t_list *stack_b);
+
+/* COMBOS */
 void		ft_do_rarb(t_list **a, t_list **b, int a_idx, int b_idx);
 void		ft_do_rrarb(t_list **a, t_list **b, int a_idx, int b_idx);
 void		ft_do_rarrb(t_list **a, t_list **b, int a_idx, int b_idx);
