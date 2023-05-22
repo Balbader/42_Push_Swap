c76cb0f521333a63691e37b3b34fbc68ed5569f0
balbader <basilalbapro@gmail.com>
2023-05-22 08:41:29 +0200

    ft_find_cheapest_node.c

---
+++ srcs/cost/ft_find_cheapest_node.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_cheapest_node.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 08:23:44 by baalbade          #+#    #+#             */
+/*   Updated: 2023/05/22 08:23:46 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*ft_find_cheapest_node(t_list **a, t_list **b)
+{
+	t_list	*cheapest_node;
+	int		cheapest_cost;
+
+	cheapest_node = (*b);
+	cheapest_cost = ft_find_cheapest_cost(a, b,
+			ft_find_big_bro_idx(a, cheapest_node->pos), cheapest_node->index);
+	while (*b)
+	{
+		if (cheapest_cost >= ft_find_cheapest_cost(a, b,
+				ft_find_big_bro_idx(a, (*b)->pos), (*b)->index))
+		{
+			cheapest_cost = ft_find_cheapest_cost(a, b,
+					ft_find_big_bro_idx(a, (*b)->pos), (*b)->index);
+			cheapest_node = (*b);
+		}
+		*b = (*b)->next;
+	}
+	return (cheapest_node);
+}
